debad3ad4758bfc96687c4bfb847fb553818d64c
Bert Timmerman <bert.timmerman@xs4all.nl>
2007-10-14 11:01:56 +0200

    modified:   pcb-fpw/src/libfpw.c

--- pcb-fpw/src/libfpw.c
+++ pcb-fpw/src/libfpw.c
@@ -17,16 +17,21 @@
 
 #define VERSION "0.0.1"
 
-/* Some package types */
-#define DIP_TYPE 10
-#define SIP_TYPE 11
-#define PGA_TYPE 12
-#define BGA_TYPE 20
-#define SMT_TYPE 21
-#define SO_TYPE 22
-#define PLCC_TYPE 23
-#define QFN_TYPE 24
-#define QFP_TYPE 25
+/* Some through hole package types */
+#define TH 100 /*!< Generic technology description. */
+#define SIP 101 /*!< Single Inline Package. */
+#define DIP 102 /*!< Dual Inline Package. */
+#define PGA 103 /*!< Pin Gate Array package. */
+#define DO 104 /*!< Diode Outline package. */
+#define TO 105 /*!< Transistor Outline package. */
+
+/* Some surface mounted package types */
+#define SMT 200 /*!< Generic technology description. */
+#define BGA 201 /*!< Ball Gate Array package. */
+#define SO 202 /*!< Small Outline package. */
+#define PLCC 203 /*!< Plastic Leadless Chip Carrier package. */
+#define QFN 204 /*!< Quad Flat No-leads package. */
+#define QFP 205 /*!< Quad Flat Package. */
 
 /* Locations for pin #1 orientation */
 /*   2 -- 5 -- 8  */
@@ -44,10 +49,10 @@
 #define MIDDLE_RIGHT 7
 #define UPPER_RIGHT 8
 
-/* Thin lines on silkscreen are not printed in the real world */
+/* Thin lines are not printed on silkscreen (in the real world) */
 #define THIN_DRAW 1 /*!< Draw with thin lines. */
 
-/* Object flag values */
+/* Pin or pad types / Object flag values */
 #define PIN 0x0001 /*!< If set, this object is a pin.\n */
                    /*!< This flag is for internal use only. */
 #define VIA 0x0002 /*!< Likewise, for vias. */
@@ -66,40 +71,43 @@
                         /*!< Primarily used for pads used as fiducials. */
 
 gchar *footprint_filename = NULL; /*!< filename of footprint file */
+gchar *suffix = ".fp";
 gchar *footprint_name = NULL; /*!< name of footprint */
 gchar *footprint_type = NULL; /*!< type of footprint */
+gint package_type; /*!< type of package */
 gchar *footprint_units = NULL; /*!< units for footprint dimensions */
+gdouble multiplier; /*!< multiplier to convert to mils/100. */
 gchar *n1_pos = NULL; /*!< position of number 1 pin. */
-gchar *pad_shape = NULL; /*!< shape of pads/pins. */
-gchar *suffix = ".fp";
 gint n; /*!< number of pads/pins. */
 gint n_col; /*!< number of columns. */
 gint n_row; /*!< number of rows. */
-gboolean thermal; /*!< has thermal pad */
-gint type; /*!< type of footprint */
 gdouble c1; /*!< top to bottom pads/pins center-center distance. */
-gdouble c2; /*!< left to right pads/pins center-center distance. */
-gdouble courtyard_line_width; /*!< courtyard line width */
 gdouble e1; /*!< vertical spacing of pads/pins (pitch). */
-gdouble e2; /*!< horizontal spacing of pads/pins (pitch). */
 gdouble g1; /*!< top to bottom pads/pins inner-inner distance. */
+gdouble z1; /*!< top to bottom pads/pins outer-outer distance. */
+gdouble c2; /*!< left to right pads/pins center-center distance. */
+gdouble e2; /*!< horizontal spacing of pads/pins (pitch). */
 gdouble g2; /*!< left to right pads/pins inner-inner distance. */
-gdouble multiplier; /*!< multiplier to convert to mils/100. */
-gdouble r1; /*!< vertical silkscreen dimension */
-gdouble r2; /*!< horizontal silkscreen dimension */
-gdouble silkscreen_line_width; /*!< silkscreen line width */
-gdouble solder_mask_clearance; /*!< solder mask clearance of pad. */
-gdouble thermal_solder_mask_clearance; /*!< solder mask clearance of thermal pad. */
-gdouble v1; /*!< vertical courtyard dimension */
-gdouble v2; /*!< horizontal courtyard dimension */
+gdouble z2; /*!< left to right pads/pins outer-outer distance. */
 gdouble x; /*!< length of pad. */
 gdouble y; /*!< width of pad. */
-gdouble thermal_x; /*!< length of thermal pad. */
-gdouble thermal_y; /*!< width of thermal pad. */
-gdouble z1; /*!< top to bottom pads/pins outer-outer distance. */
-gdouble z2; /*!< left to right pads/pins outer-outer distance. */
 gdouble d_hole; /*!< diameter of pin hole. */
 gdouble d_pad; /*!< diameter of pin pad. */
+gchar *pad_shape = NULL; /*!< shape of pads/pins. */
+gint pin_pad_type; /*!< type of pads/pins. */
+gchar *pin_pad_flags = NULL; /*!< flags of pads/pins. */
+gdouble solder_mask_clearance; /*!< solder mask clearance of pin/pad. */
+gdouble silkscreen_line_width; /*!< silkscreen line width */
+gdouble r1; /*!< vertical silkscreen dimension */
+gdouble r2; /*!< horizontal silkscreen dimension */
+gdouble v1; /*!< vertical courtyard dimension */
+gdouble v2; /*!< horizontal courtyard dimension */
+gdouble courtyard_line_width; /*!< courtyard line width */
+gboolean thermal; /*!< has thermal pad */
+gboolean thermal_nopaste; /*!< thermal pad has no paste */
+gdouble thermal_x; /*!< length of thermal pad. */
+gdouble thermal_y; /*!< width of thermal pad. */
+gdouble thermal_solder_mask_clearance; /*!< solder mask clearance of thermal pad. */
 
 
 /*!
@@ -120,11 +128,11 @@ write_rectangular_pad
         gdouble width, /*!< width of the pad */
         gdouble clearance, /*!< clearance */
         gdouble solder_mask_clearance, /*!< solder mask clearance */
-        gint flags /*!< flags */
+        gchar flags /*!< flags */
 )
 {
         fprintf (fp,
-                "\tPad[%d %d %d %d %d %d %d \"%s\" \"%d\" 0x00000100]\n",
+                "\tPad[%d %d %d %d %d %d %d \"%s\" \"%d\" \"%s\"]\n",
                 (int) x0,
                 (int) y0,
                 (int) x1,
@@ -133,7 +141,8 @@ write_rectangular_pad
                 (int) clearance,
                 (int) solder_mask_clearance,
                 pad_name,
-                pad_number
+                pad_number,
+                flags
                 );
 }
 
@@ -250,31 +259,41 @@ write_smt_footprint ()
                 fprintf (fp, "(\n");
                 if (x > y) /* Write pads parallel to x-axis */
                 {
-                        fprintf (stdout, "Pads are drawn parallel on X-axis.\n");
+                        fprintf (stderr, "Pads are drawn parallel on X-axis.\n");
                         /* Pad #1 */
-                        fprintf (fp, "\tPad[%d %d %d %d %d %d %d \"\" \"1\" 0x00000100]\n",
-                                (int) (multiplier * -1 * (e2 + x - y) / 2), /* x0 coordinate */
-                                (int) (multiplier * (0)), /* y0-coordinate */
-                                (int) (multiplier * (-e2 + x - y) / 2), /* x1 coordinate */
-                                (int) (multiplier * (0)), /* y1-coordinate */
-                                (int) (multiplier * y), /* width of the pad */
-                                (int) (multiplier * solder_mask_clearance), /* clearance */
-                                (int) (multiplier * (y + (2 * solder_mask_clearance))) /* solder mask clearance */
-                                );
+                        write_rectangular_pad
+                        (
+                                fp, /* file pointer */
+                                1, /* pad number */
+                                "", /* pad name */
+                                multiplier * -1 * (e2 + x - y) / 2, /* x0 coordinate */
+                                0, /* y0-coordinate */
+                                multiplier * (-e2 + x - y) / 2, /* x1 coordinate */
+                                0, /* y1-coordinate */
+                                multiplier * y, /* width of the pad */
+                                multiplier * solder_mask_clearance, /* clearance */
+                                multiplier * (y + (2 * solder_mask_clearance)), /* solder mask clearance */
+                                "square" /* flags */
+                        );
                         /* Pad #2 */
-                        fprintf (fp, "\tPad[%d %d %d %d %d %d %d \"\" \"2\" 0x00000100]\n",
-                                (int) (multiplier * -1 * (-e2 + x - y) / 2), /* x0 coordinate */
-                                (int) (multiplier * (0)), /* y0-coordinate */
-                                (int) (multiplier * (e2 + x - y) / 2), /* x1 coordinate */
-                                (int) (multiplier * (0)), /* y1-coordinate */
-                                (int) (multiplier * y), /* width of the pad */
-                                (int) (multiplier * solder_mask_clearance), /* clearance */
-                                (int) (multiplier * (y + (2 * solder_mask_clearance))) /* solder mask clearance */
-                                );
+                        write_rectangular_pad
+                        (
+                                fp, /* file pointer */
+                                2, /* pad number */
+                                "", /* pad name */
+                                multiplier * -1 * (-e2 + x - y) / 2, /* x0 coordinate */
+                                0, /* y0-coordinate */
+                                multiplier * (e2 + x - y) / 2, /* x1 coordinate */
+                                0, /* y1-coordinate */
+                                multiplier * y, /* width of the pad */
+                                multiplier * solder_mask_clearance, /* clearance */
+                                multiplier * (y + (2 * solder_mask_clearance)), /* solder mask clearance */
+                                "square" /* flags */
+                        );
                 }
                 else /* write pads perpendiclar to x-axis */
                 {
-                        fprintf (stdout, "Pads are drawn perpendicular to X-axis.\n");
+                        fprintf (stderr, "Pads are drawn perpendicular to X-axis.\n");
                         /* Pad #1 */
                         fprintf (fp, "\tPad[%d %d %d %d %d %d %d \"\" \"1\" 0x00000100]\n",
                                 (int) (-multiplier * (e2 / 2)), /* x0-coordinate */
@@ -299,10 +318,10 @@ write_smt_footprint ()
                 courtyard_line_width = multiplier * courtyard_line_width;
                 write_rectangular_courtyard (fp, xmin, ymin, xmax, ymax, courtyard_line_width);
                 /* Attributes in the form "Attribute("name" "value")" */
-                fprintf (fp, "\tAttribute(\"author\" \"Bert Timmerman\")\n",
-                fprintf (fp, "\tAttribute(\"dist-license\" \"GPL\")\n",
-                fprintf (fp, "\tAttribute(\"use-license\" \"unlimited\")\n",
-                fprintf (fp, "\tAttribute(\"xxx\" \"yyy\")\n",
+                fprintf (fp, "\tAttribute(\"author\" \"Bert Timmerman\")\n");
+                fprintf (fp, "\tAttribute(\"dist-license\" \"GPL\")\n");
+                fprintf (fp, "\tAttribute(\"use-license\" \"unlimited\")\n");
+                fprintf (fp, "\tAttribute(\"xxx\" \"yyy\")\n");
                 fprintf (fp, "\n");
                 fprintf (fp, ")\n");
         }
@@ -315,36 +334,120 @@ write_smt_footprint ()
  *
  */
 int
+write_footprintwizard_file()
+{
+        FILE *fp;
+        gchar *suffix = ".fp";
+
+        /* Write global variables to footprintwizard file with .fpw suffix */
+        if (g_str_has_suffix (footprint_name, suffix))
+        {
+                /* Footprintname has a .fp suffix, add a w */
+                footprint_filename = g_strconcat (footprint_name, "w", NULL);
+        }
+        else
+        {
+                /* Footprintname has no .fp suffix, add a .fpw suffix */
+                footprint_filename = g_strconcat (footprint_name, ".fpw", NULL);
+        }
+        if (fp = g_fopen (footprint_filename, "w"))
+        {
+                fprintf (fp, "%s\n", footprint_filename);
+                fprintf (fp, "%s\n", footprint_name);
+                fprintf (fp, "%s\n", footprint_type);
+                fprintf (fp, "%s\n", footprint_units);
+                fprintf (fp, "%s\n", n1_pos);
+                fprintf (fp, "%s\n", pad_shape);
+                fprintf (fp, "%d\n", n);
+                fprintf (fp, "%d\n", n_col);
+                fprintf (fp, "%d\n", n_row);
+                fprintf (fp, "%f\n", x);
+                fprintf (fp, "%f\n", y);
+                fprintf (fp, "%f\n", d_pad);
+                fprintf (fp, "%f\n", d_hole);
+                fprintf (fp, "%f\n", c1);
+                fprintf (fp, "%f\n", g1);
+                fprintf (fp, "%f\n", z1);
+                fprintf (fp, "%f\n", c2);
+                fprintf (fp, "%f\n", g2);
+                fprintf (fp, "%f\n", z2);
+                fprintf (fp, "%f\n", e1);
+                fprintf (fp, "%f\n", e2);
+                fprintf (fp, "%f\n", solder_mask_clearance);
+                fprintf (fp, "%f\n", r1);
+                fprintf (fp, "%f\n", r2);
+                fprintf (fp, "%f\n", silkscreen_line_width);
+                fprintf (fp, "%f\n", v1);
+                fprintf (fp, "%f\n", v2);
+                fprintf (fp, "%f\n", courtyard_line_width);
+                fprintf (fp, "%d\n", thermal);
+                fprintf (fp, "%f\n", thermal_x);
+                fprintf (fp, "%f\n", thermal_y);
+                fprintf (fp, "%f\n", thermal_solder_mask_clearance);
+        }
+        close (fp);
+}
+
+/*!
+ * \brief Write a footprint based on the global variables.
+ *
+ */
+int
 write_footprint()
 {
+        /* Determine the pad shape type */
+        if (strcmp (pad_shape, "rectangular pad"))
+        {
+                g_strconcat (pin_pad_flags, "square", NULL);
+        }
+        if (strcmp (pad_shape, "rectangular pin"))
+        {
+                g_strconcat (pin_pad_flags, "square", NULL);
+        }
+        if (strcmp (pad_shape, "octagonal pad"))
+        {
+                g_strconcat (pin_pad_flags, "octagon", NULL);
+        }
+        if (strcmp (pad_shape, "octagonal pin"))
+        {
+                g_strconcat (pin_pad_flags, "octagon", NULL);
+        }
+        if (strcmp (pad_shape, "round pin, elongated"))
+        {
+                g_strconcat (pin_pad_flags, "", NULL);
+        }
         /* Determine the units type */
         if (strcmp (footprint_units, "mils")) multiplier = 100.0;
         if (strcmp (footprint_units, "mils/100")) multiplier = 1.0;
         if (strcmp (footprint_units, "mm")) multiplier = (1000 / 25.4) * 100;
         /* Determine the footprint type */
-        if (strcmp (footprint_type, "DIP")) type = DIP_TYPE;
-        if (strcmp (footprint_type, "SIP")) type = SIP_TYPE;
-        if (strcmp (footprint_type, "PGA")) type = PGA_TYPE;
-        if (strcmp (footprint_type, "BGA")) type = BGA_TYPE;
-        if (strcmp (footprint_type, "QFN")) type = QFN_TYPE;
-        if (strcmp (footprint_type, "QFP")) type = QFP_TYPE;
-        if (strcmp (footprint_type, "SO")) type = SO_TYPE;
-        if (strcmp (footprint_type, "SMT")) type = SMT_TYPE;
+        if (strcmp (footprint_type, "DIP")) package_type = DIP;
+        if (strcmp (footprint_type, "SIP")) package_type = SIP;
+        if (strcmp (footprint_type, "PGA")) package_type = PGA;
+        if (strcmp (footprint_type, "BGA")) package_type = BGA;
+        if (strcmp (footprint_type, "QFN")) package_type = QFN;
+        if (strcmp (footprint_type, "QFP")) package_type = QFP;
+        if (strcmp (footprint_type, "SO")) package_type = SO;
+        if (strcmp (footprint_type, "SMT")) package_type = SMT;
         /* Depending on the footprint type write that type of footprint file
          */
-        switch (type)
+        switch (package_type)
         {
-                case SIP_TYPE:
-                case DIP_TYPE:
-                case PGA_TYPE:
-                case BGA_TYPE:
-                case QFN_TYPE:
-                case QFP_TYPE:
-                case SO_TYPE:
-                case SMT_TYPE:
+                case TH:
+                case SIP:
+                case DIP:
+                case PGA:
+                case DO:
+                case TO:
+                case SMT:
                 {
                         write_smt_footprint();
                 }
+                case BGA:
+                case SO:
+                case PLCC:
+                case QFN:
+                case QFP:
                 default:
                 {
                         fprintf (stdout, "Warning: wrong or non-existent footprint type entered.\n");
