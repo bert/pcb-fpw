40a352b72b6751eb538aac8db8f92e21d88f408a
Bert Timmerman <bert.timmerman@xs4all.nl>
2007-10-19 10:29:30 +0200

    modified:   pcb-fpw/src/fpw.c

--- pcb-fpw/src/fpw.c
+++ pcb-fpw/src/fpw.c
@@ -18,6 +18,7 @@
 #include <stdio.h>
 
 #include <gtk/gtk.h>
+#include <stdlib.h>
 
 #include "libfpw.c"
 
@@ -33,9 +34,9 @@ gchar *program_name = NULL;
 static void
 print_version ()
 {
-        fprintf (stderr, "\nfpw version %s\n", VERSION);
+        fprintf (stderr, "\n%s version %s\n", program_name, FPW_VERSION);
         fprintf (stderr, "(C) 2007 by Bert Timmerman.\n\n");
-        exit (0);
+        exit (EXIT_SUCCESS);
 }
 
 
@@ -48,11 +49,13 @@ print_version ()
 static void
 print_usage ()
 {
-        fprintf (stderr, "\nfpw usage:\n");
-        fprintf (stderr, "fpw -h   : prints this help message and exits.\n");
-        fprintf (stderr, "fpw -V   : prints the version and exits.\n");
-        fprintf (stderr, "fpw -f fpw_filename -o footprint_name   : prints the version and exits.\n\n");
-        exit (0);
+        fprintf (stderr, "\n%s usage:", program_name);
+        fprintf (stderr, "\n----------\n");
+        fprintf (stderr, "%s -h   : prints this help message and exits.\n\n", program_name);
+        fprintf (stderr, "%s -V   : prints the version and exits.\n\n", program_name);
+        fprintf (stderr, "%s -f fpw_filename -o footprint_name   : reads the template file specified after -f and\n", program_name);
+        fprintf (stderr, "                                          writes a footprint to the file specified after -o.\n\n");
+        exit (EXIT_SUCCESS);
 }
 
 
@@ -71,7 +74,6 @@ main
 {
         FILE *fp;
         gchar *fpw_filename = NULL;
-        gchar *dummy_footprint_name = NULL;
         gchar *suffix = ".fp";
 
         /* Determine how we are called today */
@@ -92,10 +94,14 @@ main
         else
         {
                 fprintf (stderr, "I'm quiting, let me do something useful.\n");
-                exit (0);
+                exit (EXIT_FAILURE);
         }
-        /* If the footprint_filename contains a valid
-         * name that exists, read variables from the fpw file */
+        /* Read variables from the fpw file */
+        read_footprintwizard_file();
+        if (strcmp (footprint_units, "mils")) multiplier = 100.0;
+        if (strcmp (footprint_units, "mils/100")) multiplier = 1.0;
+        if (strcmp (footprint_units, "mm")) multiplier = (1000 / 25.4) * 100;
+        /* If the footprint_filename contains a valid name that exists, use it */
         if (g_str_has_suffix (footprint_name, suffix))
         {
                 /* Footprintname has a .fp suffix, do nothing */
@@ -103,54 +109,12 @@ main
         }
         else
         {
-                /* Footprintname has no .fp suffix, add a .fpw suffix */
+                /* Footprintname has no .fp suffix, add a .fp suffix */
                 footprint_filename = g_strconcat (footprint_name, ".fp", NULL);
         }
-        /* Get global variables from footprintwizard file with .fpw suffix */
-        if (fp = g_fopen (fpw_filename, "r"))
-        {
-                fscanf (fp, "%s\n", footprint_filename);
-                fscanf (fp, "%s\n", dummy_footprint_name); /* do not (re)use this value ! */
-                fscanf (fp, "%s\n", footprint_type);
-                fscanf (fp, "%s\n", footprint_units);
-                fscanf (fp, "%s\n", n1_pos);
-                fscanf (fp, "%s\n", pad_shape);
-                fscanf (fp, "%d\n", n);
-                fscanf (fp, "%d\n", n_col);
-                fscanf (fp, "%d\n", n_row);
-                fscanf (fp, "%f\n", x);
-                fscanf (fp, "%f\n", y);
-                fscanf (fp, "%f\n", d_pad);
-                fscanf (fp, "%f\n", d_hole);
-                fscanf (fp, "%f\n", c1);
-                fscanf (fp, "%f\n", g1);
-                fscanf (fp, "%f\n", z1);
-                fscanf (fp, "%f\n", c2);
-                fscanf (fp, "%f\n", g2);
-                fscanf (fp, "%f\n", z2);
-                fscanf (fp, "%f\n", e1);
-                fscanf (fp, "%f\n", e2);
-                fscanf (fp, "%f\n", solder_mask_clearance);
-                fscanf (fp, "%f\n", r1);
-                fscanf (fp, "%f\n", r2);
-                fscanf (fp, "%f\n", silkscreen_line_width);
-                fscanf (fp, "%f\n", v1);
-                fscanf (fp, "%f\n", v2);
-                fscanf (fp, "%f\n", courtyard_line_width);
-                fscanf (fp, "%d\n", thermal);
-                fscanf (fp, "%f\n", thermal_x);
-                fscanf (fp, "%f\n", thermal_y);
-                fscanf (fp, "%f\n", thermal_solder_mask_clearance);
-        }
-        close (fp);
-        if (strcmp (footprint_units, "mils")) multiplier = 100.0;
-        if (strcmp (footprint_units, "mils/100")) multiplier = 1.0;
-        if (strcmp (footprint_units, "mm")) multiplier = (1000 / 25.4) * 100;
-        /* Pass control to libfpw */
         write_footprint ();
-        /* If we get here, control is passed back from libfpw */
         fprintf (stderr, "Footprint %s is written successful.", footprint_name);
+        exit (EXIT_SUCCESS);
 }
 
 /* EOF */
-
