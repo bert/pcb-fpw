09f18b4e9c6a299c2b9e46dc36ab0f1a0fdfdd66
Bert Timmerman <bert.timmerman@xs4all.nl>
2007-10-21 20:59:03 +0200

    modified:   pcb-fpw/pcb-fpw.glade modified:   pcb-fpw/src/callbacks.c modified:   pcb-fpw/src/callbacks.h

--- pcb-fpw/src/callbacks.c
+++ pcb-fpw/src/callbacks.c
@@ -339,6 +339,14 @@ on_Z2_radiobutton_toggled              (GtkToggleButton *togglebutton,
 }
 
 
+void
+on_clear_button_clicked                (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
 /*!
  * \brief The "Close" button is clicked, close the application.
  */
@@ -347,6 +355,14 @@ on_close_button_clicked                (GtkButton       *button,
                                         gpointer         user_data)
 {
         gtk_main_quit();
+}
+
+
+void
+on_courtyard_checkbutton_toggled       (GtkToggleButton *togglebutton,
+                                        gpointer         user_data)
+{
+
 }
 
 
@@ -371,6 +387,23 @@ on_courtyard_line_width_entry_changed  (GtkEditable     *editable,
 }
 
 
+void
+on_footprint_author_entry_changed      (GtkEditable     *editable,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_footprint_dist_license_entry_changed
+                                        (GtkEditable     *editable,
+                                        gpointer         user_data)
+{
+
+}
+
+
 /*!
  * \brief The footprint name entry is changed, store in footprint_name.
  *
@@ -382,6 +415,14 @@ on_footprint_name_entry_changed        (GtkEditable     *editable,
                                         gpointer         user_data)
 {
         footprint_name = gtk_entry_get_text (GTK_EDITABLE (editable));
+}
+
+
+void
+on_footprint_status_entry_changed      (GtkComboBox     *combobox,
+                                        gpointer         user_data)
+{
+
 }
 
 
@@ -413,6 +454,14 @@ on_footprint_units_entry_changed       (GtkComboBox     *combobox,
 }
 
 
+void
+on_footprint_use_license_entry_changed (GtkEditable     *editable,
+                                        gpointer         user_data)
+{
+
+}
+
+
 /*!
  * \brief The horizontal courtyard dimension (V1) entry is changed, store in
  * v1.
@@ -430,6 +479,14 @@ on_horizontal_courtyard_entry_changed  (GtkEditable     *editable,
 
         V1_string = gtk_entry_get_text (GTK_EDITABLE (editable));
         v1 = g_ascii_strtod (V1_string, &leftovers);
+}
+
+
+void
+on_indicate_1_checkbutton_toggled      (GtkToggleButton *togglebutton,
+                                        gpointer         user_data)
+{
+
 }
 
 
@@ -460,7 +517,7 @@ on_horizontal_silkscreen_entry_changed (GtkEditable     *editable,
  * - store in the n1_pos variable (global).
  */
 void
-on_number_1_position_combobox_entry_changed
+on_number_1_position_entry_changed
                                         (GtkComboBox     *combobox,
                                         gpointer         user_data)
 {
@@ -528,6 +585,55 @@ on_number_total_pins_entry_changed     (GtkEditable     *editable,
 }
 
 
+void
+on_package_body_height_entry_changed   (GtkEditable     *editable,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_package_body_length_entry_changed   (GtkEditable     *editable,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_package_body_width_entry_changed    (GtkEditable     *editable,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_package_is_radial_checkbutton_toggled
+                                        (GtkToggleButton *togglebutton,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_package_outline_checkbutton_toggled (GtkToggleButton *togglebutton,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_pad_clearance_entry_changed         (GtkEditable     *editable,
+                                        gpointer         user_data)
+{
+
+}
+
+
 /*!
  * \brief The diameter of the pad (D) entry is changed,
  * store in d_pad.
@@ -574,7 +680,7 @@ on_pad_length_entry_changed            (GtkEditable     *editable,
  * - store in the pad_shape variable (global).
  */
 void
-on_pad_shape_combobox_entry_changed    (GtkComboBox     *combobox,
+on_pad_shape_entry_changed    (GtkComboBox     *combobox,
                                         gpointer         user_data)
 {
         pad_shape = gtk_combo_box_get_active_text (GTK_COMBO_BOX (combobox));
--- pcb-fpw/src/callbacks.h
+++ pcb-fpw/src/callbacks.h
@@ -186,3 +186,170 @@ on_refresh_button_clicked              (GtkButton       *button,
 void
 on_pcb_gfpw_destroy                    (GtkObject       *object,
                                         gpointer         user_data);
+
+void
+on_footprint_name_entry_changed        (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_footprint_type_entry_changed        (GtkComboBox     *combobox,
+                                        gpointer         user_data);
+
+void
+on_package_body_length_entry_changed   (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_package_body_width_entry_changed    (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_package_body_height_entry_changed   (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_footprint_author_entry_changed      (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_footprint_dist_license_entry_changed
+                                        (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_footprint_use_license_entry_changed (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_footprint_status_entry_changed      (GtkComboBox     *combobox,
+                                        gpointer         user_data);
+
+void
+on_radial_package_checkbutton_toggled  (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_package_is_radial_checkbutton_toggled
+                                        (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_close_button_clicked                (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_clear_button_clicked                (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_save_button_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_refresh_button_clicked              (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_number_total_pins_entry_changed     (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_pad_length_entry_changed            (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_pad_width_entry_changed             (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_pad_diameter_entry_changed          (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_pin_drill_diameter_entry_changed    (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_number_of_rows_entry_changed        (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_number_of_columns_entry_changed     (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_E2_entry_changed                    (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_E1_entry_changed                    (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_pad_shape_entry_changed             (GtkComboBox     *combobox,
+                                        gpointer         user_data);
+
+void
+on_number_1_position_entry_changed     (GtkComboBox     *combobox,
+                                        gpointer         user_data);
+
+void
+on_number_1_position_entry_changed     (GtkComboBox     *combobox,
+                                        gpointer         user_data);
+
+void
+on_pad_shape_entry_changed             (GtkComboBox     *combobox,
+                                        gpointer         user_data);
+
+void
+on_pad_clearance_entry_changed         (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_pad_solder_mask_clearance_entry_changed
+                                        (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_thermal_checkbutton_toggled         (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_thermal_width_entry_changed         (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_thermal_length_entry_changed        (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_thermal_nopaste_checkbutton_toggled (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_silkscreen_line_width_entry_changed (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_package_outline_checkbutton_toggled (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_indicate_1_checkbutton_toggled      (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_courtyard_checkbutton_toggled       (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_horizontal_courtyard_entry_changed  (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_vertical_courtyard_entry_changed    (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_courtyard_line_width_entry_changed  (GtkEditable     *editable,
+                                        gpointer         user_data);
