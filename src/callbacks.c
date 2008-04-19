36628801559e8cfd922e6de20f6f4157a1bc070e
Bert Timmerman <bert.timmerman@xs4all.nl>
2008-04-19 10:06:14 +0200

    Set GUI constraints for the PGA package type.

--- src/callbacks.c
+++ src/callbacks.c
@@ -391,6 +391,52 @@ gui_constraints_set_bga (GtkWidget *widget)
 
 
 /*!
+ * \brief Set GUI constraints for the PGA package type.
+ */
+int
+gui_constraints_set_pga (GtkWidget *widget)
+{
+        /* Widgets on tab 1 "Footprint" */
+        GtkWidget *package_is_radial_checkbutton = lookup_widget (GTK_WIDGET (widget),
+                "package_is_radial_checkbutton");
+        gtk_toggle_button_set_active (package_is_radial_checkbutton, FALSE);
+        gtk_widget_set_sensitive (package_is_radial_checkbutton, FALSE);
+
+        /* Widgets on tab 2 "Pins/Pads" */
+        GtkWidget *count_x_entry = lookup_widget (GTK_WIDGET (widget),
+                "count_x_entry");
+        gtk_entry_set_text (GTK_WIDGET (count_x_entry), "");
+        gtk_widget_set_sensitive (count_x_entry, FALSE);
+        GtkWidget *count_y_entry = lookup_widget (GTK_WIDGET (widget),
+                "count_y_entry");
+        gtk_entry_set_text (GTK_WIDGET (count_y_entry), "");
+        gtk_widget_set_sensitive (count_y_entry, FALSE);
+        GtkWidget *number_1_position_entry = lookup_widget (GTK_WIDGET (widget),
+                "number_1_position_entry");
+        gtk_combo_box_set_active (GTK_WIDGET (number_1_position_entry), 1);
+        gtk_widget_set_sensitive (number_1_position_entry, FALSE);
+        GtkWidget *pad_shape_entry = lookup_widget (GTK_WIDGET (widget),
+                "pad_shape_entry");
+        gtk_combo_box_set_active (GTK_WIDGET (pad_shape_entry), 1);
+        gtk_widget_set_sensitive (pad_shape_entry, FALSE);
+        GtkWidget *pad_length_entry = lookup_widget (GTK_WIDGET (widget),
+                "pad_length_entry");
+        gtk_entry_set_text (GTK_WIDGET (pad_length_entry), "");
+        gtk_widget_set_sensitive (pad_length_entry, FALSE);
+        GtkWidget *pad_width_entry = lookup_widget (GTK_WIDGET (widget),
+                "pad_width_entry");
+        gtk_entry_set_text (GTK_WIDGET (pad_width_entry), "");
+        gtk_widget_set_sensitive (pad_width_entry, FALSE);
+
+        /* Widgets on tab 3 "Thermal Pad" */
+        gui_constraints_disable_thermal_tab_widgets (widget);
+
+        /* Widgets on tab 5 "Heel & Toe goals" */
+        gui_constraints_disable_heel_and_toe_goals_tab_widgets (widget);
+}
+
+
+/*!
  * \brief Send a message to the statusbar.
  */
 int
@@ -1370,6 +1416,7 @@ on_footprint_type_entry_changed        (GtkComboBox     *combobox,
         else if (!strcmp (footprint_type, "PGA"))
         {
                 all_entries_to_default_sensitivity (combobox);
+                gui_constraints_set_pga (combobox);
                 package_type = PGA;
                 return;
         }
