e7061422a7a48eda00fd5bd022f07b4f034995f8
Bert Timmerman <bert.timmerman@xs4all.nl>
2008-05-31 14:45:08 +0200

    Enabling NLS and inserted gettext macros.

--- src/fpw.c
+++ src/fpw.c
@@ -31,6 +31,33 @@
 #  include <config.h>
 #endif
 
+/*
+ * Standard gettext macros.
+ */
+#ifdef ENABLE_NLS
+#  include <libintl.h>
+#  undef _
+#  define _(String) dgettext (PACKAGE, String)
+#  define Q_(String) g_strip_context ((String), gettext (String))
+#  ifdef gettext_noop
+#    define N_(String) gettext_noop (String)
+#  else
+#    define N_(String) (String)
+#  endif
+#else
+#  define textdomain(String) (String)
+#  define gettext(String) (String)
+#  define dgettext(Domain,Message) (Message)
+#  define dcgettext(Domain,Message,Type) (Message)
+#  define bindtextdomain(Domain,Directory) (Domain)
+#  define _(String) (String)
+#  define Q_(String) g_strip_context ((String), (String))
+#  define N_(String) (String)
+        bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
+        bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+        textdomain (GETTEXT_PACKAGE);
+#endif
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
