9ab8c53c23c6b3bdf182aae5b9d5521eb780a7db
Bert Timmerman <bert.timmerman@xs4all.nl>
2008-07-16 23:04:11 +0200

    Added some new line markers in the doxygen documentation.

--- src/callbacks.c
+++ src/callbacks.c
@@ -7,7 +7,7 @@
  * files to be used by the pcb layout application
  * (see http://pcb.sourgeforge.net) for the placement of parts in a pcb
  * layout.\n
- *
+ * \n
  * This program is free software; you can redistribute it and/or modify\n
  * it under the terms of the GNU General Public License as published by\n
  * the Free Software Foundation; either version 2 of the License, or\n
@@ -15,7 +15,7 @@
  * \n
  * This program is distributed in the hope that it will be useful,\n
  * but WITHOUT ANY WARRANTY; without even the implied warranty of\n
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n
  * See the GNU General Public License for more details.\n
  * \n
  * You should have received a copy of the GNU General Public License\n
