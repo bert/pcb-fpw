ed0d85b3b52ecf280e481eb45dcf03cb3b829b58
Bert Timmerman <bert.timmerman@xs4all.nl>
2008-08-12 22:49:20 +0200

    Added a BGA100C50P15X15_1000X1000X120 package.

--- src/bga.c
+++ src/bga.c
@@ -29,7 +29,7 @@
  * Currently the following footprints are supported:\n
  * BGA100C100P10X10_1100X1100X140, BGA100C100P10X10_1100X1100X170,
  * BGA100C100P10X10_1100X1100X350, BGA100C50P11X11_600X600X100,
- * BGA100C50P14X14_800X800X120,
+ * BGA100C50P14X14_800X800X120, BGA100C50P15X15_1000X1000X120,
  */
 int
 bga_get_default_footprint_values
@@ -200,6 +200,53 @@ bga_get_default_footprint_values
                 silkscreen_width = 8.00;
                 g_free (footprint_name);
                 footprint_name = g_strdup ("BGA100C50P14X14_800X800X120");
+                return (EXIT_SUCCESS);
+        }
+        else if (!strcmp (footprint_name, "?BGA100C50P15X15_1000X1000X120"))
+        {
+                footprint_units = g_strdup ("mm");
+                number_of_pins = 100;
+                pin_pad_exception_string = g_strconcat
+                (
+                        "B2,B14,",
+                        "C3,C4,C5,C6,C7,C8,C9,C10,C11,C12,C13,",
+                        "D3,D4,D5,D6,D7,D8,D9,D10,D11,D12,D13,",
+                        "E3,E4,E5,E6,E7,E8,E9,E10,E11,E12,E13,",
+                        "F3,F4,F5,F6,F7,F8,F9,F10,F11,F12,F13,",
+                        "G3,G4,G5,G6,G7,G8,G9,G10,G11,G12,G13,",
+                        "H3,H4,H5,H6,H7,H8,H9,H10,H11,H12,H13,",
+                        "J3,J4,J5,J6,J7,J8,J9,J10,J11,J12,J13,",
+                        "K3,K4,K5,K6,K7,K8,K9,K10,K11,K12,K13,",
+                        "L3,L4,L5,L6,L7,L8,L9,L10,L11,L12,L13,",
+                        "M3,M4,M5,M6,M7,M8,M9,M10,M11,M12,M13,",
+                        "N3,N4,N5,N6,N7,N8,N9,N10,N11,N12,N13,",
+                        "P2,P14,",
+                        NULL
+                );
+                package_body_width = 10.00;
+                package_body_length = 10.00;
+                package_body_height = 1.20;
+                package_is_radial = FALSE;
+                number_of_columns = 15;
+                number_of_rows = 15;
+                pitch_x = 0.50;
+                pitch_y = 0.50;
+                count_x = 0;
+                count_y = 0;
+                pad_shape = g_strdup ("circular pad");
+                pin_drill_diameter = 0.0;
+                pad_diameter = 0.25;
+                pad_clearance = 0.15;
+                pad_solder_mask_clearance = 0.15;
+                fiducial = TRUE;
+                fiducial_pad_diameter = 1.00;
+                fiducial_pad_solder_mask_clearance = 1.00;
+                courtyard_length = 11.00;
+                courtyard_width = 11.00;
+                silkscreen_length = 10.00;
+                silkscreen_width = 10.00;
+                g_free (footprint_name);
+                footprint_name = g_strdup ("BGA100C50P15X15_1000X1000X120");
                 return (EXIT_SUCCESS);
         }
         else
