ff10dbcf4a05a8363122cc1c24d40964489dfc50
Bert Timmerman <bert.timmerman@xs4all.nl>
2008-08-16 21:12:57 +0200

    Moved gui_constraints_set_pga() to pga_set_gui_constraints().

--- src/callbacks.c
+++ src/callbacks.c
@@ -38,6 +38,7 @@
 #include "libfpw.c"
 #include "bga.c"
 #include "dip.c"
+#include "pga.c"
 #include "sot.c"
 #include "preview.c"
 
@@ -826,52 +827,6 @@ gui_constraints_set_con_hdr (GtkWidget *widget)
                 "number_1_position_entry");
         gtk_combo_box_set_active (GTK_COMBO_BOX (number_1_position_entry), 1);
         gtk_widget_set_sensitive (number_1_position_entry, FALSE);
-
-        /* Widgets on tab 3 "Thermal Pad" */
-        gui_constraints_disable_thermal_tab_widgets (widget);
-
-        /* Widgets on tab 5 "Heel & Toe goals" */
-        gui_constraints_disable_heel_and_toe_goals_tab_widgets (widget);
-}
-
-
-/*!
- * \brief Set GUI constraints for the PGA package type.
- */
-int
-gui_constraints_set_pga (GtkWidget *widget)
-{
-        /* Widgets on tab 1 "Footprint" */
-        GtkWidget *package_is_radial_checkbutton = lookup_widget (GTK_WIDGET (widget),
-                "package_is_radial_checkbutton");
-        gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (package_is_radial_checkbutton), FALSE);
-        gtk_widget_set_sensitive (package_is_radial_checkbutton, FALSE);
-
-        /* Widgets on tab 2 "Pins/Pads" */
-        GtkWidget *count_x_entry = lookup_widget (GTK_WIDGET (widget),
-                "count_x_entry");
-        gtk_entry_set_text (GTK_ENTRY (count_x_entry), "");
-        gtk_widget_set_sensitive (count_x_entry, FALSE);
-        GtkWidget *count_y_entry = lookup_widget (GTK_WIDGET (widget),
-                "count_y_entry");
-        gtk_entry_set_text (GTK_ENTRY (count_y_entry), "");
-        gtk_widget_set_sensitive (count_y_entry, FALSE);
-        GtkWidget *number_1_position_entry = lookup_widget (GTK_WIDGET (widget),
-                "number_1_position_entry");
-        gtk_combo_box_set_active (GTK_COMBO_BOX (number_1_position_entry), 1);
-        gtk_widget_set_sensitive (number_1_position_entry, FALSE);
-        GtkWidget *pad_shape_entry = lookup_widget (GTK_WIDGET (widget),
-                "pad_shape_entry");
-        gtk_combo_box_set_active (GTK_COMBO_BOX (pad_shape_entry), 1);
-        gtk_widget_set_sensitive (pad_shape_entry, FALSE);
-        GtkWidget *pad_length_entry = lookup_widget (GTK_WIDGET (widget),
-                "pad_length_entry");
-        gtk_entry_set_text (GTK_ENTRY (pad_length_entry), "");
-        gtk_widget_set_sensitive (pad_length_entry, FALSE);
-        GtkWidget *pad_width_entry = lookup_widget (GTK_WIDGET (widget),
-                "pad_width_entry");
-        gtk_entry_set_text (GTK_ENTRY (pad_width_entry), "");
-        gtk_widget_set_sensitive (pad_width_entry, FALSE);
 
         /* Widgets on tab 3 "Thermal Pad" */
         gui_constraints_disable_thermal_tab_widgets (widget);
@@ -2345,7 +2300,7 @@ on_footprint_type_entry_changed        (GtkComboBox     *combobox,
                 case PGA:
                 {
                         all_entries_to_default_sensitivity (GTK_WIDGET (combobox));
-                        gui_constraints_set_pga (GTK_WIDGET (combobox));
+                        pga_set_gui_constraints (GTK_WIDGET (combobox));
                         break;
                 }
                 case PLCC:
