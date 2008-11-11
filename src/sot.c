53e0be549c453a504e099b65bdf19188fdee047b
Bert Timmerman <bert.timmerman@xs4all.nl>
2008-11-11 22:28:06 +0100

    Added SOT65P280X145-8AN packet.

--- src/sot.c
+++ src/sot.c
@@ -38,6 +38,7 @@
  * - SOT65P275X110-8N,
  * - SOT65P280X100-8N,
  * - SOT65P280X130-8N,
+ * - SOT65P280X145-8AN,
  * - SOT80P330X140-8N,
  * - SOT95P228X102-3N,
  * - SOT95P230X109-3N,
@@ -345,6 +346,37 @@ sot_get_default_footprint_values
                 g_log ("", G_LOG_LEVEL_WARNING,
                         _("SOT65P280X130-8N: side of pads is being trimmed to maintain pad to pad clearance."));
                 footprint_name = g_strdup ("SOT65P280X130-8N");
+                return (EXIT_SUCCESS);
+        }
+        else if (!strcmp (footprint_name, "?SOT65P280X145-8AN"))
+        {
+                pitch_y = 0.65;
+                number_of_pins = 8;
+                package_body_length = 1.75;
+                package_body_width = 3.05;
+                package_body_height = 1.45;
+                package_is_radial = FALSE;
+                number_of_columns = 2;
+                number_of_rows = 4;
+                pitch_x = 2.50;
+                pad_length = 1.20;
+                pad_width = 0.45;
+                pad_shape = g_strdup ("circular pad, elongated");
+                pad_shapes_type = ROUND_ELONGATED;
+                thermal = FALSE;
+                thermal_length = 0.0;
+                thermal_width = 0.0;
+                silkscreen_length = 0.50;
+                silkscreen_width = 3.00;
+                courtyard_length = 4.20;
+                courtyard_width = 3.60;
+                count_x = 0;
+                count_y = 0;
+                footprint_units = g_strdup ("mm");
+                g_free (footprint_name);
+                g_log ("", G_LOG_LEVEL_WARNING,
+                        _("SOT65P280X145-8AN: side of pads is being trimmed to maintain pad to pad clearance."));
+                footprint_name = g_strdup ("SOT65P280X145-8AN");
                 return (EXIT_SUCCESS);
         }
         else if (!strcmp (footprint_name, "?SOT80P330X140-8N"))
