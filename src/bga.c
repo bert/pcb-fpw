e471b5beb80b88a3b734205442c94af9b21226cc
lambert <ljh4timm@xs4all.nl>
2009-02-05 21:29:07 +0100

    Added data for a BGA1152C100P34X34_3500X3500X332 package.

--- src/bga.c
+++ src/bga.c
@@ -392,6 +392,7 @@ bga_create_packages_list ()
         bga_packages_list = g_list_append (bga_packages_list, "BGA114C80P6X19_550X1600X140");
         bga_packages_list = g_list_append (bga_packages_list, "BGA114C80P6X19_550X1600X150");
         bga_packages_list = g_list_append (bga_packages_list, "BGA1152C100P34X34_3500X3500X244");
+        bga_packages_list = g_list_append (bga_packages_list, "BGA1152C100P34X34_3500X3500X332");
         bga_packages_list = g_list_append (bga_packages_list, "BGA1152C100P34X34_3500X3500X340");
         bga_packages_list = g_list_append (bga_packages_list, "BGA1152C100P34X34_3500X3500X350");
         bga_packages_list = g_list_append (bga_packages_list, "BGA1156C100P34X34_3500X3500X260");
@@ -715,6 +716,7 @@ bga_drc ()
  * - BGA114C80P6X19_550X1600X140,
  * - BGA114C80P6X19_550X1600X150,
  * - BGA1152C100P34X34_3500X3500X244,
+ * - BGA1152C100P34X34_3500X3500X332,
  * - BGA1152C100P34X34_3500X3500X340,
  * - BGA1152C100P34X34_3500X3500X350,
  * - BGA1156C100P34X34_3500X3500X260,
@@ -2871,6 +2873,43 @@ bga_get_default_footprint_values
                         _("BGA1152C100P34X34_3500X3500X244: is also known as JEDEC MO-034AAR-1."));
                 g_free (footprint_name);
                 footprint_name = g_strdup ("BGA1152C100P34X34_3500X3500X244");
+                return (EXIT_SUCCESS);
+        }
+        else if (!strcmp (footprint_name, "?BGA1152C100P34X34_3500X3500X332"))
+        {
+                footprint_units = g_strdup ("mm");
+                number_of_pins = 1148;
+                pin_pad_exceptions_string = g_strconcat
+                (
+                        "A1,A34,",
+                        "AP1,AP34,",
+                        NULL
+                );
+                package_body_width = 35.00;
+                package_body_length = 35.00;
+                package_body_height = 3.32;
+                package_is_radial = FALSE;
+                number_of_columns = 34;
+                number_of_rows = 34;
+                pitch_x = 1.00;
+                pitch_y = 1.00;
+                count_x = 0;
+                count_y = 0;
+                pad_shape = g_strdup ("circular pad");
+                pin_drill_diameter = 0.0;
+                pad_diameter = 0.50;
+                pad_clearance = 0.075;
+                pad_solder_mask_clearance = 0.075;
+                fiducial = FALSE;
+                fiducial_pad_diameter = 0.00;
+                fiducial_pad_solder_mask_clearance = 0.00;
+                courtyard_length = 39.00;
+                courtyard_width = 39.00;
+                silkscreen_length = 35.00;
+                silkscreen_width = 35.00;
+                silkscreen_line_width = 0.20;
+                g_free (footprint_name);
+                footprint_name = g_strdup ("BGA1152C100P34X34_3500X3500X332");
                 return (EXIT_SUCCESS);
         }
         else if (!strcmp (footprint_name, "?BGA1152C100P34X34_3500X3500X340"))
