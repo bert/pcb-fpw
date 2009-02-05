64d5ed8b950c9c7cfa5572acb5624b893e3666d8
lambert <ljh4timm@xs4all.nl>
2009-02-05 20:49:04 +0100

    Added data for a BGA1152C100P34X34_3500X3500X244 package.

--- src/bga.c
+++ src/bga.c
@@ -2814,6 +2814,45 @@ bga_get_default_footprint_values
                 footprint_name = g_strdup ("BGA114C80P6X19_550X1600X150");
                 return (EXIT_SUCCESS);
         }
+        else if (!strcmp (footprint_name, "?BGA1152C100P34X34_3500X3500X244"))
+        {
+                footprint_units = g_strdup ("mm");
+                number_of_pins = 1148;
+                pin_pad_exceptions_string = g_strconcat
+                (
+                        "A1,A34,",
+                        "AP1,AP34,",
+                        NULL
+                );
+                package_body_width = 35.00;
+                package_body_length = 35.00;
+                package_body_height = 2.44;
+                package_is_radial = FALSE;
+                number_of_columns = 34;
+                number_of_rows = 34;
+                pitch_x = 1.00;
+                pitch_y = 1.00;
+                count_x = 0;
+                count_y = 0;
+                pad_shape = g_strdup ("circular pad");
+                pin_drill_diameter = 0.0;
+                pad_diameter = 0.50;
+                pad_clearance = 0.075;
+                pad_solder_mask_clearance = 0.075;
+                fiducial = FALSE;
+                fiducial_pad_diameter = 0.00;
+                fiducial_pad_solder_mask_clearance = 0.00;
+                courtyard_length = 39.00;
+                courtyard_width = 39.00;
+                silkscreen_length = 35.00;
+                silkscreen_width = 35.00;
+                silkscreen_line_width = 0.20;
+                g_log ("", G_LOG_LEVEL_INFO,
+                        _("BGA1152C100P34X34_3500X3500X244: is also known as JEDEC MO-034AAR-1."));
+                g_free (footprint_name);
+                footprint_name = g_strdup ("BGA1152C100P34X34_3500X3500X244");
+                return (EXIT_SUCCESS);
+        }
         else if (!strcmp (footprint_name, "?BGA1152C100P34X34_3500X3500X340"))
         {
                 footprint_units = g_strdup ("mm");
