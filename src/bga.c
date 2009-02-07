2612dd03dc172bf2963348dbeb045afb6d4999b7
lambert <ljh4timm@xs4all.nl>
2009-02-07 23:44:21 +0100

    Added data for a BGA120C50P13X13_700X700X140 package.

--- src/bga.c
+++ src/bga.c
@@ -409,6 +409,7 @@ bga_create_packages_list ()
         bga_packages_list = g_list_append (bga_packages_list, "BGA119C127P7X17_1400X2200X240");
         bga_packages_list = g_list_append (bga_packages_list, "BGA120C50P11X11_600X600X100");
         bga_packages_list = g_list_append (bga_packages_list, "BGA120C50P11X11_600X600X80");
+        bga_packages_list = g_list_append (bga_packages_list, "BGA120C50P13X13_700X700X140");
         return (*bga_packages_list);
 }
 
@@ -743,6 +744,7 @@ bga_drc ()
  * - BGA119C127P7X17_1400X2200X240,
  * - BGA120C50P11X11_600X600X100,
  * - BGA120C50P11X11_600X600X80,
+ * - BGA120C50P13X13_700X700X140,
  */
 int
 bga_get_default_footprint_values
@@ -3577,6 +3579,49 @@ bga_get_default_footprint_values
                 silkscreen_line_width = 0.20;
                 g_free (footprint_name);
                 footprint_name = g_strdup ("BGA120C50P11X11_600X600X80");
+                return (EXIT_SUCCESS);
+        }
+        else if (!strcmp (footprint_name, "?BGA120C50P13X13_700X700X140"))
+        {
+                footprint_units = g_strdup ("mm");
+                number_of_pins = 120;
+                pin_pad_exceptions_string = g_strconcat
+                (
+                        "D4,D5,D6,D7,D8,D9,D10,",
+                        "E4,E5,E6,E7,E8,E9,E10,",
+                        "F4,F5,F6,F7,F8,F9,F10,",
+                        "G4,G5,G6,G7,G8,G9,G10,",
+                        "H4,H5,H6,H7,H8,H9,H10,",
+                        "J4,J5,J6,J7,J8,J9,J10,",
+                        "K4,K5,K6,K7,K8,K9,K10,",
+                        NULL
+                );
+                package_body_width = 7.00;
+                package_body_length = 7.00;
+                package_body_height = 1.40;
+                package_is_radial = FALSE;
+                number_of_columns = 13;
+                number_of_rows = 13;
+                pitch_x = 0.50;
+                pitch_y = 0.50;
+                count_x = 0;
+                count_y = 0;
+                pad_shape = g_strdup ("circular pad");
+                pin_drill_diameter = 0.0;
+                pad_diameter = 0.25;
+                pad_clearance = 0.075;
+                pad_solder_mask_clearance = 0.075;
+                fiducial = TRUE;
+                fiducial_pad_diameter = 1.00;
+                fiducial_pad_solder_mask_clearance = 1.00;
+                courtyard_length = 9.00;
+                courtyard_width = 9.00;
+                courtyard_line_width = 0.05;
+                silkscreen_length = 7.00;
+                silkscreen_width = 7.00;
+                silkscreen_line_width = 0.20;
+                g_free (footprint_name);
+                footprint_name = g_strdup ("BGA120C50P13X13_700X700X140");
                 return (EXIT_SUCCESS);
         }
         else
