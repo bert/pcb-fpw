a5ce56d9a3c67ac701f2b66dc6cc935dc474d2f9
Bert Timmerman <bert.timmerman@xs4all.nl>
2009-02-15 20:32:29 +0100

    Updated the copyright notice.

--- src/bga.c
+++ src/bga.c
@@ -1,6 +1,6 @@
 /*!
  * \file bga.c
- * \author Copyright (C) 2008 by Bert Timmerman <bert.timmerman@xs4all.nl>
+ * \author Copyright (C) 2008 .. 2009 by Bert Timmerman <bert.timmerman@xs4all.nl>
  * \brief Functions for BGA footprints.
  *
  * This program is free software; you can redistribute it and/or modify
