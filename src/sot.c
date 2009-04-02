a479cec13ea2f59672e83d68d35d168a8e5e9b78
Bert Timmerman <bert.timmerman@xs4all.nl>
2009-04-02 22:52:30 +0200

    Corrections on the SOT95P240X114-3N package [#10].

--- src/sot.c
+++ src/sot.c
@@ -1500,8 +1500,8 @@ sot_get_default_footprint_values
         else if (!strcmp (footprint_name, "?SOT95P240X114-3N"))
         {
                 /* SOT95P240X114-3N
-                 * SOT, 0.95mm pitch;
-                 * 3 pin, 1.40mm W X 3.00mm L X 1.14mm H body
+                 * SOT, 0.95 mm pitch;
+                 * 3 pin, 1.40 mm W X 3.00 mm L X 1.14 mm H body
                  * Fairchild SOT-23 FS-AU */
                 pitch_y = 0.95;
                 number_of_pins = 3;
@@ -1511,25 +1511,26 @@ sot_get_default_footprint_values
                 package_is_radial = FALSE;
                 number_of_columns = 2;
                 number_of_rows = 3;
-/*! \todo Look up datasheet and check values for SOT95P240X114-3N. */
-/*
-                pitch_x = ;
-                pad_length = ;
-                pad_width = ;
-                silkscreen_length = ;
-                silkscreen_width = ;
-                courtyard_length = ;
-                courtyard_width = ;
-*/
+                pitch_x = 2.30;
+                pad_length = 1.00;
+                pad_width = 0.60;
                 pad_shape = g_strdup ("rectangular pad");
                 pad_shapes_type = SQUARE;
                 thermal = FALSE;
                 thermal_length = 0.0;
                 thermal_width = 0.0;
+                silkscreen_length = 0.65;
+                silkscreen_width = 2.90;
+                silkscreen_line_width = 0.20;
+                courtyard_length = 3.80;
+                courtyard_width = 3.50;
+                courtyard_line_width = 0.05;
                 count_x = 0;
                 count_y = 0;
                 footprint_units = g_strdup ("mm");
                 g_free (footprint_name);
+                g_log ("", G_LOG_LEVEL_WARNING,
+                        _("SOT95P240X114-3N: side of pads is being trimmed to maintain pad to pad clearance."));
                 footprint_name = g_strdup ("SOT95P240X114-3N");
                 return (EXIT_SUCCESS);
         }
