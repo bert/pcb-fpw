51e7623bb9db084b167a5b3f2dc8f73719ce80d8
Bert Timmerman <bert.timmerman@xs4all.nl>
2009-04-04 10:11:00 +0200

    Corrections on the SOT95P280X100-6N package [#10].

--- src/sot.c
+++ src/sot.c
@@ -1763,8 +1763,8 @@ sot_get_default_footprint_values
         else if (!strcmp (footprint_name, "?SOT95P280X100-6N"))
         {
                 /* SOT95P280X100-6N
-                 * SOT, 0.95mm pitch;
-                 * 6 pin, 1.75mm W X 3.00mm L X 1.00mm H Body
+                 * SOT, 0.95 mm pitch;
+                 * 6 pin, 1.75 mm W X 3.00 mm L X 1.00 mm H Body
                  * JEDEC MO-193 Linear 6-Lead Plastic TSOT-23 S6 Package */
                 pitch_y = 0.95;
                 number_of_pins = 6;
@@ -1774,22 +1774,28 @@ sot_get_default_footprint_values
                 package_is_radial = FALSE;
                 number_of_columns = 2;
                 number_of_rows = 3;
-                pitch_x = 2.40;
-                pad_length = 1.35;
+                pitch_x = 2.70;
+                pad_length = 1.10;
                 pad_width = 0.55;
                 pad_shape = g_strdup ("rectangular pad");
                 pad_shapes_type = SQUARE;
                 thermal = FALSE;
                 thermal_length = 0.0;
                 thermal_width = 0.0;
-                silkscreen_length = 0.40;
-                silkscreen_width = 3.00;
+                silkscreen_length = 0.95;
+                silkscreen_width = 2.90;
+                silkscreen_line_width = 0.20;
                 courtyard_length = 4.30;
                 courtyard_width = 3.50;
+                courtyard_line_width = 0.05;
                 count_x = 0;
                 count_y = 0;
                 footprint_units = g_strdup ("mm");
                 g_free (footprint_name);
+                g_log ("", G_LOG_LEVEL_INFO,
+                        _("SOT95P280X100-6N: is also known as JEDEC MO-193."));
+                g_log ("", G_LOG_LEVEL_WARNING,
+                        _("SOT95P280X100-6N: pads are being trimmed to prevent extension under the package body."));
                 footprint_name = g_strdup ("SOT95P280X100-6N");
                 return (EXIT_SUCCESS);
         }
