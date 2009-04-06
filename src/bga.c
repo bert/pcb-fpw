cff1dd4c095a06e38c638fca0f790c836d48d577
Bert Timmerman <bert.timmerman@xs4all.nl>
2009-04-07 00:26:51 +0200

    Corrections on the BGA119C127P7X17_1400X2200X196 package [#30].

--- src/bga.c
+++ src/bga.c
@@ -3468,8 +3468,8 @@ bga_get_default_footprint_values
                 footprint_units = g_strdup ("mm");
                 number_of_pins = 119;
                 pin_pad_exceptions_string = g_strdup ("");
-                package_body_width = 14.00;
-                package_body_length = 22.00;
+                package_body_length = 14.00;
+                package_body_width = 22.00;
                 package_body_height = 1.96;
                 package_is_radial = FALSE;
                 number_of_columns = 7;
