3e7922d88b1c7904a95d5913039ff1dfe614d798
Bert Timmerman <bert.timmerman@xs4all.nl>
2009-04-07 00:33:27 +0200

    Corrections on the BGA121C50P14X14_1000X1300X130 package [#30].

--- src/bga.c
+++ src/bga.c
@@ -4026,8 +4026,8 @@ bga_get_default_footprint_values
                         "P1,P2,P13,P14,",
                         NULL
                 );
-                package_body_width = 10.00;
-                package_body_length = 13.00;
+                package_body_length = 10.00;
+                package_body_width = 13.00;
                 package_body_height = 1.30;
                 package_is_radial = FALSE;
                 number_of_columns = 14;
