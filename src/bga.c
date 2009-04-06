c1583f3e201b28f9ab66168e8ae133b32196a741
Bert Timmerman <bert.timmerman@xs4all.nl>
2009-04-07 00:12:52 +0200

    Corrections on the BGA107C80P10X14_1050X1300X120 package [#30].

--- src/bga.c
+++ src/bga.c
@@ -2114,8 +2114,8 @@ bga_get_default_footprint_values
                         "P3,P4,P5,P6,P7,P8,",
                         NULL
                 );
-                package_body_width = 10.50;
-                package_body_length = 13.00;
+                package_body_length = 10.50;
+                package_body_width = 13.00;
                 package_body_height = 1.20;
                 package_is_radial = FALSE;
                 number_of_columns = 10;
