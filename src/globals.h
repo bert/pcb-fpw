5373e93c89a9f6125c16f1c13b7e517494fc4f0d
Bert Timmerman <bert.timmerman@xs4all.nl>
2009-06-28 21:05:27 +0200

    Set "Add license to footprint" to be initially TRUE.

--- src/globals.h
+++ src/globals.h
@@ -213,7 +213,7 @@ gchar *footprint_status = "Experimental";
 status_t status_type = NO_STATUS;
         /*!< Status (enumerated) for the footprint status.\n
          * Initial value is \c NO_STATUS. */
-gboolean license_in_footprint;
+gboolean license_in_footprint = TRUE;
         /*!< Include a license text into the footprint file. */
 gboolean attributes_in_footprint;
         /*!< Include the global values based on the entry values of the GUI
