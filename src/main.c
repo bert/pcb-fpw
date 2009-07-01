70cd6682ac5ea56f457c8c44e6e9529e194394d4
Bert Timmerman <bert.timmerman@xs4all.nl>
2009-07-01 08:23:28 +0200

    Get the Doxygen stuff for main.c right this time ?

--- src/main.c
+++ src/main.c
@@ -8,20 +8,20 @@
  * (see http://pcb.sourgeforge.net) for the placement of parts in a pcb
  * layout.\n
  *
- * This program is free software; you can redistribute it and/or modify\n
- * it under the terms of the GNU General Public License as published by\n
- * the Free Software Foundation; either version 2 of the License, or\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.\n
  * \n
- * This program is distributed in the hope that it will be useful,\n
- * but WITHOUT ANY WARRANTY; without even the implied warranty of\n
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.\n
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.\n
  * \n
- * You should have received a copy of the GNU General Public License\n
- * along with this program; if not, write to:\n
- * the Free Software Foundation, Inc.,\n
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.\n
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.\n
  */
 
 #ifdef HAVE_CONFIG_H
@@ -34,8 +34,17 @@
 #include "support.h"
 
 
+/*!
+ * \brief The GTK GUI for the footprintwizard.
+ *
+ * \return 0 if successfull.
+ */
 int
-main (int argc, char *argv[])
+main
+(
+        int argc, /*!< : number of arguments.*/
+        char *argv[] /*!< : array of argument variables.*/
+)
 {
         GtkWidget *pcb_gfpw;
         GtkWidget *about_dialog;
