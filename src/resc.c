c95eab5d79cc107bd0abdc38b63faba3c71f88af
Bert Timmerman <bert.timmerman@xs4all.nl>
2009-07-27 15:02:29 +0200

    Get the Doxygen stuff for resc.c right this time ?

--- src/resc.c
+++ src/resc.c
@@ -1,22 +1,22 @@
 /*!
  * \file resc.c
- * \author Copyright (C) 2008 by Bert Timmerman <bert.timmerman@xs4all.nl>
+ * \author Copyright (C) 2008 ... 2009 by Bert Timmerman <bert.timmerman@xs4all.nl>
  * \brief Functions for RESC SMT footprints.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.\n
- *
+ * \n
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n
- * See the GNU General Public License for more details.\n
- *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.\n
+ * \n
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to:\n
- * the Free Software Foundation, Inc., \n
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.\n
  */
 
 #include "register_functions.c"
@@ -29,12 +29,16 @@
  * If the footprint name is recognised known values will be loaded and the
  * entries involved in the GUI will be updated accordingly.\n
  * Currently the following footprints are supported:\n
- * - .
+ * -.
+ *
+ * \return \c EXIT_SUCCESS when default values for a footprint were
+ * found, \c EXIT_FAILURE when the footprint name was not found.
  */
 int
 resc_get_default_footprint_values
 (
         gchar *footprint_name
+                /*!< : a \c NULL terminated footprint name.*/
 )
 {
         if (!strcmp (footprint_name, "?RESC"))
@@ -80,6 +84,10 @@ resc_get_default_footprint_values
 #if GUI
 /*!
  * \brief Set GUI constraints for the RESC SMT package type.
+ *
+ * This function is only to be compiled for GUI targets.
+ *
+ * \return \c EXIT_SUCCESS when the function is completed.
  */
 int
 resc_set_gui_constraints (GtkWidget *widget)
@@ -114,12 +122,16 @@ resc_set_gui_constraints (GtkWidget *widget)
 
         /* Widgets on tab 5 "Heel & Toe goals" */
         gui_constraints_disable_heel_and_toe_goals_tab_widgets (widget);
+        return (EXIT_SUCCESS);
 }
 #endif /* GUI */
 
 
 /*!
  * \brief Write a RESC SMT footprint with two pads for a chip package.
+ *
+ * \return \c EXIT_FAILURE when errors were encountered,
+ * \c EXIT_SUCCESS when OK.
  */
 int
 resc_write_footprint ()
@@ -271,6 +283,9 @@ resc_write_footprint ()
 }
 
 
+/*!
+ * \brief A list containing all RESC related functions.
+ */
 static fpw_function_t
 resc_function_list[] =
 {
@@ -297,8 +312,14 @@ resc_function_list[] =
 };
 
 
+/*!
+ * \brief A list containing all RESC related functions.
+ */
 REGISTER_FUNCTIONS (resc_function_list)
 
+/*!
+ * \brief Initialise by registering all RESC related functions.
+ */
 void
 resc_init ()
 {
