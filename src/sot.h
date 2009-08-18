f9dbfc4b5d06e31d7360c269ab4c37e16b3fd728
Bert Timmerman <bert.timmerman@xs4all.nl>
2009-08-18 19:06:31 +0200

    Added a header file for sot.c

---
+++ src/sot.h
@@ -0,0 +1,39 @@
+/*!
+ * \file sot.h
+ * \author Copyright 2009 by Bert Timmerman <bert.timmerman@xs4all.nl>
+ * \brief Header file for SOT footprints functions.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.\n
+ * \n
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.\n
+ * \n
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.\n
+ */
+
+
+#ifndef __SOT_INCLUDED__
+#define __SOT_INCLUDED__
+
+#include "globals.h"
+
+
+/* Forward declaration of functions. */
+ElementTypePtr *sot_create_element ();
+GList sot_create_packages_list ();
+int sot_drc ();
+int sot_get_default_footprint_values (gchar *);
+int sot_set_gui_constraints (GtkWidget *);
+int sot_write_footprint ();
+void sot_init ();
+
+
+#endif /* __SOT_INCLUDED__ */
