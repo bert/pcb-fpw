36402e59985eb9a3421503b1f5e474e28c2cd0b6
lambert <ljh4timm@xs4all.nl>
2009-11-08 21:30:17 +0100

    Added data for a CAPC3215X168N package [#21].

--- src/capc.c
+++ src/capc.c
@@ -333,6 +333,7 @@ capc_create_packages_list ()
         capc_packages_list = g_list_append (capc_packages_list, "CAPC2012X94N");
         capc_packages_list = g_list_append (capc_packages_list, "CAPC2012X95N");
         capc_packages_list = g_list_append (capc_packages_list, "CAPC2013X140N");
+        capc_packages_list = g_list_append (capc_packages_list, "CAPC3215X168N");
         capc_packages_list = g_list_append (capc_packages_list, "CAPC3216X105L");
         return (*capc_packages_list);
 }
@@ -659,6 +660,7 @@ capc_drc ()
  * - CAPC2012X94N,
  * - CAPC2012X95N,
  * - CAPC2013X140N,
+ * - CAPC23215168N,
  * - CAPC3216X105L.
  *
  * \return EXIT_SUCCESS when default values for a footprint were found,
@@ -1795,6 +1797,39 @@ capc_get_default_footprint_values
                 g_log ("", G_LOG_LEVEL_WARNING,
                         _("CAPC2013X140N: Footprint is too small for a package outline on the silkscreen."));
                 footprint_name = g_strdup ("CAPC2013X140N");
+                return (EXIT_SUCCESS);
+        }
+        else if (!strcmp (footprint_name, "?CAPC3215X168N"))
+        {
+                number_of_pins = 2;
+                package_body_length = 3.40;
+                package_body_width = 1.60;
+                package_body_height = 1.68;
+                package_is_radial = FALSE;
+                number_of_columns = 0;
+                number_of_rows = 0;
+                pitch_x = 3.00;
+                pitch_y = 0.0;
+                pad_length = 1.15;
+                pad_width = 1.65;
+                pad_shape = g_strdup ("rectangular pad");
+                pad_shapes_type = SQUARE;
+                pad_clearance = 0.075;
+                pad_solder_mask_clearance = 0.075;
+                silkscreen_package_outline = TRUE;
+                silkscreen_length = 1.10;
+                silkscreen_width = 1.50;
+                silkscreen_line_width = 0.20;
+                courtyard_length = 4.70;
+                courtyard_width = 2.20;
+                courtyard_line_width = 0.05;
+                count_x = 0;
+                count_y = 0;
+                footprint_units = g_strdup ("mm");
+                g_free (footprint_name);
+                g_log ("", G_LOG_LEVEL_INFO,
+                        _("CAPC3215X168N: is also known as EIA 1206, metric 3216."));
+                footprint_name = g_strdup ("CAPC3215X168N");
                 return (EXIT_SUCCESS);
         }
         else if (!strcmp (footprint_name, "?CAPC3216X105L"))
