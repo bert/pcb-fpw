da5c50d5e71d86697d7c2fc216a06e9bab73f4f0
Bert Timmerman <bert.timmerman@xs4all.nl>
2009-11-15 10:26:33 +0100

    Write attributes to file and finish con_dil_write_footprint() graceful.

--- src/con_dil.c
+++ src/con_dil.c
@@ -338,6 +338,23 @@ con_dil_write_footprint ()
                         multiplier * courtyard_line_width
                 );
         }
+        /* Write attributes to the footprint file. */
+        if (attributes_in_footprint)
+        {
+                write_attributes ();
+        }
+        /* Finishing touch. */
+        fprintf (fp, "\n");
+        fprintf (fp, ")\n");
+        fclose (fp);
+        /* We are ready creating a footprint. */
+        if (verbose)
+        {
+                g_log ("", G_LOG_LEVEL_INFO,
+                        _("wrote a footprint for a %s package: %s."),
+                        footprint_type,
+                        footprint_filename);
+        }
         return (EXIT_SUCCESS);
 }
 
