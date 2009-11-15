fa5eac041cc9ab760926ba8a01ee8b5ba4f99ac8
Bert Timmerman <bert.timmerman@xs4all.nl>
2009-11-15 10:08:35 +0100

    Write attributes to file and finish capm_write_footprint() graceful.

--- src/capm.c
+++ src/capm.c
@@ -1018,6 +1018,23 @@ capm_write_footprint ()
                         multiplier * courtyard_line_width
                 );
         }
+        /* Write attributes to the footprint file. */
+        if (attributes_in_footprint)
+        {
+                write_attributes ();
+        }
+        /* Finishing touch. */
+        fprintf (fp, "\n");
+        fprintf (fp, ")\n");
+        fclose (fp);
+        /* We are ready creating a footprint. */
+        if (verbose)
+        {
+                g_log ("", G_LOG_LEVEL_INFO,
+                        _("created an element for a %s package: %s."),
+                        footprint_type,
+                        footprint_filename);
+        }
         return (EXIT_SUCCESS);
 }
 
