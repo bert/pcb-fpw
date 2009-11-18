f6082c625bf81c7077283f4168b5a9780a5d997d
Bert Timmerman <bert.timmerman@xs4all.nl>
2009-11-18 07:48:27 +0100

    Contain all file handling for writing a CAPMP footprint file in capmp_write_footprint().

--- src/capmp.c
+++ src/capmp.c
@@ -801,6 +801,21 @@ capmp_write_footprint ()
         gdouble x_text;
         gdouble y_text;
 
+        /* Attempt to open a file with write permission. */
+        fp = fopen (footprint_filename, "w");
+        if (!fp)
+        {
+                g_log ("", G_LOG_LEVEL_WARNING,
+                        _("could not open file for %s footprint: %s."),
+                        footprint_type, footprint_filename);
+                fclose (fp);
+                return (EXIT_FAILURE);
+        }
+        /* Print a license if requested. */
+        if (license_in_footprint)
+        {
+                write_license ();
+	}
         /* Determine (extreme) courtyard dimensions based on pin/pad
          * properties */
         xmin = multiplier * ((-pitch_x / 2.0) - (pad_length / 2.0) - pad_solder_mask_clearance);
