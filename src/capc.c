1a00c20aee00bfc292a828b9e1ca78afbc3a4bf1
Bert Timmerman <bert.timmerman@xs4all.nl>
2009-12-03 08:03:37 +0100

    Declare pin_pad_flags as a local variable.

--- src/capc.c
+++ src/capc.c
@@ -1945,6 +1945,7 @@ capc_write_footprint ()
         gdouble ymin;
         gdouble x_text;
         gdouble y_text;
+        gchar *pin_pad_flags = g_strdup ("");
 
         /* Attempt to open a file with write permission. */
         fp = fopen (footprint_filename, "w");
