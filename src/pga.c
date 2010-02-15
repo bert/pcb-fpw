12444968b23980651d22c58354373a1d387c83de
Bert Timmerman <bert.timmerman@xs4all.nl>
2010-02-15 08:17:54 +0100

    Correction in comments.

--- src/pga.c
+++ src/pga.c
@@ -627,13 +627,13 @@ pga_set_gui_constraints
                 /*!< : the caller widget.*/
 )
 {
-        /* Widgets on tab 1 "Footprint" */
+        /* Widgets on tab "Footprint" */
         GtkWidget *package_is_radial_checkbutton = lookup_widget (GTK_WIDGET (widget),
                 "package_is_radial_checkbutton");
         gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (package_is_radial_checkbutton), FALSE);
         gtk_widget_set_sensitive (package_is_radial_checkbutton, FALSE);
 
-        /* Widgets on tab 2 "Pins/Pads" */
+        /* Widgets on tab "Pins/Pads" */
         GtkWidget *count_x_entry = lookup_widget (GTK_WIDGET (widget),
                 "count_x_entry");
         gtk_entry_set_text (GTK_ENTRY (count_x_entry), "");
@@ -662,10 +662,10 @@ pga_set_gui_constraints
         gtk_entry_set_text (GTK_ENTRY (pad_width_entry), "");
         gtk_widget_set_sensitive (pad_width_entry, FALSE);
 
-        /* Widgets on tab 3 "Thermal Pad" */
+        /* Widgets on tab "Thermal Pad" */
         gui_constraints_disable_thermal_tab_widgets (widget);
 
-        /* Widgets on tab 5 "Heel & Toe goals" */
+        /* Widgets on tab "Heel & Toe goals" */
         gui_constraints_disable_heel_and_toe_goals_tab_widgets (widget);
         return (EXIT_SUCCESS);
 }
