ddd663564c76332d2d502b42598fb2fe059140ed
Bert Timmerman <bert.timmerman@xs4all.nl>
2010-02-28 15:31:07 +0100

    Changes in file description for doxygen.

--- src/con_hdr.c
+++ src/con_hdr.c
@@ -1,7 +1,8 @@
 /*!
  * \file con_hdr.c
  * \author Copyright 2007, 2008, 2009 by Bert Timmerman <bert.timmerman@xs4all.nl>
- * \brief Functions for CON-HDR (connector) footprints.
+ * \brief Functions for CON-HDR footprints
+ * (Header Connector).
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/con_hdr.h
+++ src/con_hdr.h
@@ -1,7 +1,8 @@
 /*!
  * \file con_hdr.h
  * \author Copyright 2007, 2008, 2009, 2010 by Bert Timmerman <bert.timmerman@xs4all.nl>
- * \brief Header file for functions for CON-HDR (connector) footprints.
+ * \brief Header file for functions for CON-HDR footprints
+ * (Header Connector).
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
