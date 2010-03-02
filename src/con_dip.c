0f5b7cf45782e7f5885019b402b0a05cee9796bf
Bert Timmerman <bert.timmerman@xs4all.nl>
2010-03-02 22:48:45 +0100

    More translatable strings encapsulated.
    Fixed a typo in a comment.

--- src/con_dip.c
+++ src/con_dip.c
@@ -835,7 +835,7 @@ con_dip_write_footprint ()
         if (!fp)
         {
                 g_log ("", G_LOG_LEVEL_WARNING,
-                        _("could not open file for %s footprint: %s."),
+                        (_("could not open file for %s footprint: %s.")),
                         footprint_type, footprint_filename);
                 fclose (fp);
                 return (EXIT_FAILURE);
@@ -984,20 +984,20 @@ con_dip_write_footprint ()
         /* Write a package body on the silkscreen */
         if (silkscreen_package_outline)
         {
-                fprintf (fp, "# Write a package body on the silkscreen\n");
+                fprintf (fp, (_("# Write a package body on the silkscreen\n")));
                 write_rectangle
                 (
                         multiplier * ((-package_body_length) / 2.0), /* xmin-coordinate */
                         multiplier * ((-package_body_width) / 2.0), /* ymin-coordinate */
                         multiplier * ((package_body_length) / 2.0), /* xmax-coordinate */
-                        multiplier * ((package_body_width) / 2.0), /* ymax-coordiante */
+                        multiplier * ((package_body_width) / 2.0), /* ymax-coordinate */
                         multiplier * silkscreen_line_width
                 );
         }
         /* Write a pin #1 marker on the silkscreen */
         if (silkscreen_indicate_1)
         {
-                fprintf (fp, "# Write a pin 1 marker on the silkscreen\n");
+                fprintf (fp, (_("# Write a pin 1 marker on the silkscreen\n")));
                 /* Write a marker around pin #1 inside the package outline */
                 write_element_line
                 (
@@ -1047,7 +1047,7 @@ con_dip_write_footprint ()
         /* Write a courtyard on the silkscreen */
         if (courtyard)
         {
-                fprintf (fp, "# Write a courtyard on the silkscreen\n");
+                fprintf (fp, (_("# Write a courtyard on the silkscreen\n")));
                 write_rectangle
                 (
                         xmin, /* already in mil/100 */
@@ -1070,7 +1070,7 @@ con_dip_write_footprint ()
         if (verbose)
         {
                 g_log ("", G_LOG_LEVEL_INFO,
-                        _("wrote a footprint for a %s package: %s."),
+                        (_("wrote a footprint for a %s package: %s.")),
                         footprint_type,
                         footprint_filename);
         }
