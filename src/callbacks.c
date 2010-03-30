4218ef3aae1cc0c6f06f431e7c4c462a1578999b
lambert <ljh4timm@xs4all.nl>
2010-03-30 07:29:52 +0200

    Implement case of SIP package in on_footprint_name_entry_changed().

--- src/callbacks.c
+++ src/callbacks.c
@@ -2811,6 +2811,12 @@ on_footprint_name_entry_changed        (GtkEditable     *editable,
                                         all_entries_need_updated (GTK_WIDGET (editable));
                                 return;
                         }
+                        case SIP:
+                        {
+                                if (sip_get_default_footprint_values (footprint_name) == EXIT_SUCCESS)
+                                        all_entries_need_updated (GTK_WIDGET (editable));
+                                return;
+                        }
                         case SOT:
                         {
                                 if (sot_get_default_footprint_values (footprint_name) == EXIT_SUCCESS)
