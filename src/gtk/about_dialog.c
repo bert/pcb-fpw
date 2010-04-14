3e4daef557b89a8a104fa41aa088407762a57595
lambert <ljh4timm@xs4all.nl>
2010-04-14 23:56:48 +0200

    Avoid duplication of identical defines [#34].

--- src/gtk/about_dialog.c
+++ src/gtk/about_dialog.c
@@ -34,15 +34,6 @@
 #include <gtk/gtk.h>
 
 
-#define GLADE_HOOKUP_OBJECT(component,widget,name) \
-        g_object_set_data_full (G_OBJECT (component), name, \
-        gtk_widget_ref (widget), (GDestroyNotify) gtk_widget_unref)
-
-
-#define GLADE_HOOKUP_OBJECT_NO_REF(component,widget,name) \
-        g_object_set_data (G_OBJECT (component), name, widget)
-
-
 /*!
  * \brief The close button of the about dialog is clicked.
  *
