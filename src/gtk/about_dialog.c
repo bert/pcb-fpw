9d7bead1c8dd8835bb10b4df6b16f3177a476777
lambert <ljh4timm@xs4all.nl>
2010-04-25 14:04:50 +0200

    Correction in indentation style [#34].

--- src/gtk/about_dialog.c
+++ src/gtk/about_dialog.c
@@ -82,44 +82,45 @@ on_about_dialog_destroy                (GtkDialog       *dialog,
 GtkWidget*
 create_about_dialog (void)
 {
-  GtkWidget *about_dialog;
-  const gchar *authors[] = {
-    "Bert Timmerman <bert.timmerman@xs4all.nl>",
-    NULL
-  };
-  /* TRANSLATORS: Replace this string with your names, one name per line. */
-  gchar *translators = "Bert Timmerman (nl)";
-  GdkPixbuf *about_dialog_logo_pixbuf;
+        GtkWidget *about_dialog;
+        const gchar *authors[] =
+        {
+                "Bert Timmerman <bert.timmerman@xs4all.nl>",
+                NULL
+        };
+        /* TRANSLATORS: Replace this string with your names, one name per line. */
+        gchar *translators = "Bert Timmerman (nl)";
+        GdkPixbuf *about_dialog_logo_pixbuf;
 
-  about_dialog = gtk_about_dialog_new ();
-  gtk_widget_set_name (about_dialog, "about_dialog");
-  gtk_about_dialog_set_version (GTK_ABOUT_DIALOG (about_dialog), VERSION);
-  gtk_about_dialog_set_name (GTK_ABOUT_DIALOG (about_dialog), _("pcb-gfpw"));
-  gtk_about_dialog_set_copyright (GTK_ABOUT_DIALOG (about_dialog), _("(C) 2007, 2008, 2009, 2010 Bert Timmerman."));
-  gtk_about_dialog_set_comments (GTK_ABOUT_DIALOG (about_dialog), _("The pcb FootPrintWizard generates footprint files for the pcb layout editor.\n(see http://pcb.gpleda.org)\n"));
-  gtk_about_dialog_set_license (GTK_ABOUT_DIALOG (about_dialog), _("This program is free software; you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation; either version 2 of the License, or\n(at your option) any later version.\n\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\nSee the GNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with this program; if not, write to:\nthe Free Software Foundation, Inc.,\n51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.\n"));
-  gtk_about_dialog_set_website (GTK_ABOUT_DIALOG (about_dialog), "http://www.xs4all.nl/~ljh4timm/pcb-fpw/pcb-fpw.html");
-  gtk_about_dialog_set_authors (GTK_ABOUT_DIALOG (about_dialog), authors);
-  gtk_about_dialog_set_translator_credits (GTK_ABOUT_DIALOG (about_dialog), translators);
-  about_dialog_logo_pixbuf = create_pixbuf ("splash_wiz.xpm");
-  gtk_about_dialog_set_logo (GTK_ABOUT_DIALOG (about_dialog), about_dialog_logo_pixbuf);
-  g_signal_connect
-  (
-          (gpointer) about_dialog,
-          "close",
-          G_CALLBACK (on_about_dialog_close),
-          NULL
-  );
-  g_signal_connect
-  (
-          (gpointer) about_dialog,
-          "destroy",
-          G_CALLBACK (on_about_dialog_destroy),
-          NULL
-  );
-  /* Store pointers to all widgets, for use by lookup_widget(). */
-  GLADE_HOOKUP_OBJECT_NO_REF (about_dialog, about_dialog, "about_dialog");
-  return about_dialog;
+        about_dialog = gtk_about_dialog_new ();
+        gtk_widget_set_name (about_dialog, "about_dialog");
+        gtk_about_dialog_set_version (GTK_ABOUT_DIALOG (about_dialog), VERSION);
+        gtk_about_dialog_set_name (GTK_ABOUT_DIALOG (about_dialog), _("pcb-gfpw"));
+        gtk_about_dialog_set_copyright (GTK_ABOUT_DIALOG (about_dialog), _("(C) 2007, 2008, 2009, 2010 Bert Timmerman."));
+        gtk_about_dialog_set_comments (GTK_ABOUT_DIALOG (about_dialog), _("The pcb FootPrintWizard generates footprint files for the pcb layout editor.\n(see http://pcb.gpleda.org)\n"));
+        gtk_about_dialog_set_license (GTK_ABOUT_DIALOG (about_dialog), _("This program is free software; you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation; either version 2 of the License, or\n(at your option) any later version.\n\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\nSee the GNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with this program; if not, write to:\nthe Free Software Foundation, Inc.,\n51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.\n"));
+        gtk_about_dialog_set_website (GTK_ABOUT_DIALOG (about_dialog), "http://www.xs4all.nl/~ljh4timm/pcb-fpw/pcb-fpw.html");
+        gtk_about_dialog_set_authors (GTK_ABOUT_DIALOG (about_dialog), authors);
+        gtk_about_dialog_set_translator_credits (GTK_ABOUT_DIALOG (about_dialog), translators);
+        about_dialog_logo_pixbuf = create_pixbuf ("splash_wiz.xpm");
+        gtk_about_dialog_set_logo (GTK_ABOUT_DIALOG (about_dialog), about_dialog_logo_pixbuf);
+        g_signal_connect
+        (
+                (gpointer) about_dialog,
+                "close",
+                G_CALLBACK (on_about_dialog_close),
+                NULL
+        );
+        g_signal_connect
+        (
+                (gpointer) about_dialog,
+                "destroy",
+                G_CALLBACK (on_about_dialog_destroy),
+                NULL
+        );
+        /* Store pointers to all widgets, for use by lookup_widget(). */
+        GLADE_HOOKUP_OBJECT_NO_REF (about_dialog, about_dialog, "about_dialog");
+        return about_dialog;
 }
 
 
