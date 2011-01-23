28787f23d12ca4c5e99f5ce4ce3384283bda5c47
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-01-23 16:28:33 +0100

    RES: added data for a RES2210-1700X450 package.

--- src/res.c
+++ src/res.c
@@ -433,6 +433,7 @@ res_create_packages_list (GList *res_packages_list)
         res_packages_list = g_list_append (res_packages_list, "RES1860-1350X500");
         res_packages_list = g_list_append (res_packages_list, "RES2080-1600X550");
         res_packages_list = g_list_append (res_packages_list, "RES2160-1650X650");
+        res_packages_list = g_list_append (res_packages_list, "RES2210-1700X450");
         return (*res_packages_list);
 }
 
@@ -738,6 +739,7 @@ res_drc ()
  * - RES1860-1350X500,
  * - RES2080-1600X550,
  * - RES2160-1650X650,
+ * - RES2210-1700X450,
  * .
  *
  * \return \c EXIT_SUCCESS when default values for a footprint were
@@ -1322,6 +1324,44 @@ res_get_default_footprint_values
                 courtyard_line_width = 0.05;
                 g_free (footprint_name);
                 footprint_name = g_strdup ("RES2160-1650X650");
+                return (EXIT_SUCCESS);
+        }
+        else if (!strcmp (footprint_name, "?RES2210-1700X450"))
+        {
+                footprint_units = g_strdup ("mm");
+                package_body_length = 17.00;
+                package_body_width = 4.50;
+                package_body_height = 4.50;
+                package_is_radial = FALSE;
+                number_of_pins = 2;
+                number_of_columns = 2;
+                number_of_rows = 1;
+                pitch_x = 22.10;
+                pitch_y = 0.00;
+                count_x = 0;
+                count_y = 0;
+                pad_diameter = 1.52;
+                pad_length = 0.00;
+                pad_width = 0.00;
+                pad_shape = g_strdup ("circular pad");
+                pad_shapes_type = ROUND;
+                pin_drill_diameter = 1.00;
+                thermal = FALSE;
+                thermal_length = 0.0;
+                thermal_width = 0.0;
+                fiducial = FALSE;
+                fiducial_pad_diameter = 0.00;
+                fiducial_pad_solder_mask_clearance = 0.00;
+                silkscreen_package_outline = FALSE;
+                silkscreen_length = 17.00;
+                silkscreen_width = 4.50;
+                silkscreen_line_width = 0.20;
+                courtyard = FALSE;
+                courtyard_length = pitch_x + pad_diameter + pad_solder_mask_clearance;
+                courtyard_width = 4.50;
+                courtyard_line_width = 0.05;
+                g_free (footprint_name);
+                footprint_name = g_strdup ("RES2210-1700X450");
                 return (EXIT_SUCCESS);
         }
         else
