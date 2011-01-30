16aa94df127d94649dada2d0b1647d34fed70c65
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-01-30 23:25:42 +0100

    CAPMP: correction in DRC for silkscreen linewidth.

--- src/capmp.c
+++ src/capmp.c
@@ -619,7 +619,7 @@ capmp_drc ()
          * the solder fillet area or worse). */
 
         /* Check for a reasonable silk line width. */
-        if (silkscreen_package_outline || (silkscreen_line_width == 0.0))
+        if (silkscreen_package_outline && (silkscreen_line_width == 0.0))
         {
                 if (verbose)
                 {
@@ -643,7 +643,7 @@ capmp_drc ()
                         break;
                 }
                 case MIL:
-                if (silkscreen_line_width > 40.0)
+                if (silkscreen_package_outline && (silkscreen_line_width > 40.0))
                 {
                         if (verbose)
                         {
@@ -655,7 +655,7 @@ capmp_drc ()
                         break;
                 }
                 case MIL_100:
-                if (silkscreen_line_width > 4000.0)
+                if (silkscreen_package_outline && (silkscreen_line_width > 4000.0))
                 {
                         if (verbose)
                         {
@@ -667,7 +667,7 @@ capmp_drc ()
                         break;
                 }
                 case MM:
-                if (silkscreen_line_width > 1.0)
+                if (silkscreen_package_outline && (silkscreen_line_width > 1.0))
                 {
                         if (verbose)
                         {
