739f170290503a7cef49b57676a4f92131722ccc
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-01-30 20:21:26 +0100

    CAPM: check for pitch in Y-direction to be 0.

--- src/capm.c
+++ src/capm.c
@@ -567,13 +567,13 @@ capm_drc ()
                 }
                 result = EXIT_FAILURE;
         }
-        /* Check for minimum clearance between copper (Y-direction). */
-        if (pitch_y - pad_width < pad_clearance)
+        /* Check for pitch in Y-direction to be 0.0 */
+        if (pitch_y != 0.0)
         {
                 if (verbose)
                 {
                         g_log ("", G_LOG_LEVEL_WARNING,
-                                (_("[%s] DRC Error: minimum clearance between copper (Y-direction) is too small.")),
+                                (_("[%s] DRC Error: pitch in Y-direction should be 0.")),
                                 footprint_type);
                 }
                 result = EXIT_FAILURE;
