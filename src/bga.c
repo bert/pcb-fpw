60bc05d0e1487efcd1a30c18961a378c64fbd831
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-01-31 08:02:15 +0100

    BGA: added a comment.

--- src/bga.c
+++ src/bga.c
@@ -642,6 +642,7 @@ bga_drc ()
                 }
                 result = EXIT_FAILURE;
         }
+        /* Check for fiducials */
         if (fiducial)
         {
                 /* Check for a zero width fiducial pad. */
