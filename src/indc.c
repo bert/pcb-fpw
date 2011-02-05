bcf01efd217de1e246353f5444d62371d2ff8484
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-02-05 09:36:48 +0100

    INDC: removed a dangling doxygen todo comment.

--- src/indc.c
+++ src/indc.c
@@ -633,7 +633,6 @@ indc_drc ()
                         break;
                 }
         }
-        /*! \todo Create attributes here. */
         /* No failures on DRC found. */
         if (verbose || (result == EXIT_SUCCESS))
         {
