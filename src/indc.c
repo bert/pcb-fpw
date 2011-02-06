879dfb01d9a05f485ca59e66f5a5436180c5d283
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-02-06 12:05:21 +0100

    INDC: added data for a INDC4648X310N package.

--- src/indc.c
+++ src/indc.c
@@ -311,6 +311,7 @@ indc_create_packages_list ()
         indc_packages_list = g_list_append (indc_packages_list, "INDC3225X135N");
         indc_packages_list = g_list_append (indc_packages_list, "INDC4509X190N");
         indc_packages_list = g_list_append (indc_packages_list, "INDC4532X175N");
+        indc_packages_list = g_list_append (indc_packages_list, "INDC4648X310N");
         return (*indc_packages_list);
 }
 
@@ -632,6 +633,7 @@ indc_drc ()
  * - INDC3225X135N,
  * - INDC4509X190N,
  * - INDC4532X175N,
+ * - INDC4648X310N,
  * .
  *
  * \return \c EXIT_SUCCESS when default values for a footprint were
@@ -876,6 +878,38 @@ indc_get_default_footprint_values
                                 (_("%s is also known as")), footprint_name,
                                 " EIA 1812, metric 4532.");
                 }
+                return (EXIT_SUCCESS);
+        }
+        else if (!strcmp (footprint_name, "?INDC4648X310N"))
+        {
+                footprint_units = g_strdup ("mm");
+                package_body_length = 4.60;
+                package_body_width = 5.00;
+                package_body_height = 3.10;
+                package_is_radial = FALSE;
+                number_of_pins = 2;
+                number_of_columns = 0;
+                number_of_rows = 0;
+                count_x = 0;
+                count_y = 0;
+                pitch_x = 3.50;
+                pitch_y = 0.0;
+                pad_length = 2.00;
+                pad_width = 5.00;
+                pad_shape = g_strdup ("rectangular pad");
+                pad_shapes_type = SQUARE;
+                thermal = FALSE;
+                thermal_length = 0.0;
+                thermal_width = 0.0;
+                silkscreen_package_outline = TRUE;
+                silkscreen_length = 0.20;
+                silkscreen_width = 4.80;
+                silkscreen_line_width = 0.20;
+                courtyard_length = 6.00;
+                courtyard_width = 5.50;
+                courtyard_line_width = 0.05;
+                g_free (footprint_name);
+                footprint_name = g_strdup ("INDC4648X310N");
                 return (EXIT_SUCCESS);
         }
         else
