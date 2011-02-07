1b4d1f953760c51fd4cf3fefa7baededb39020c4
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-02-07 22:51:48 +0100

    CAPA: correction in DRC success message.

--- src/capa.c
+++ src/capa.c
@@ -708,7 +708,7 @@ capa_drc ()
                 }
         }
         /* No failures on DRC found. */
-        if (verbose || (result == EXIT_SUCCESS))
+        if (verbose && (result == EXIT_SUCCESS))
         {
                 g_log ("", G_LOG_LEVEL_INFO,
                         (_("[%s] DRC Check: no errors while checking package %s.")),
