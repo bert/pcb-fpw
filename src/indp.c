725045ccf3144d20a70315d2f931441b51d0c2e5
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-02-07 22:50:18 +0100

    INDP: correction in DRC success message.

--- src/indp.c
+++ src/indp.c
@@ -686,7 +686,7 @@ indp_drc ()
                 }
         }
         /* No failures on DRC found. */
-        if (verbose || (result == EXIT_SUCCESS))
+        if (verbose && (result == EXIT_SUCCESS))
         {
                 g_log ("", G_LOG_LEVEL_INFO,
                         (_("[%s] DRC Check: no errors while checking package %s.")),
