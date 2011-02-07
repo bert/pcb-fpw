3ff0febb80bd92d69cef96b81500cc1e43ab6a50
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-02-07 22:56:33 +0100

    CAPPR: correction in DRC success message.

--- src/cappr.c
+++ src/cappr.c
@@ -725,7 +725,7 @@ cappr_drc ()
                 }
         }
         /* No failures on DRC found. */
-        if (verbose || (result == EXIT_SUCCESS))
+        if (verbose && (result == EXIT_SUCCESS))
         {
                 g_log ("", G_LOG_LEVEL_INFO,
                         (_("[%s] DRC Check: no errors while checking package %s.")),
