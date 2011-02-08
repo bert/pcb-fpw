d445311b70c4f33d42c316a371d9d4e82189617b
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-02-08 08:05:20 +0100

    SOT: correction in DRC success message.

--- src/sot.c
+++ src/sot.c
@@ -919,7 +919,7 @@ sot_drc ()
         }
         /*! \todo Create attributes here. */
         /* No failures on DRC found. */
-        if (verbose || (result == EXIT_SUCCESS))
+        if (verbose && (result == EXIT_SUCCESS))
         {
                 g_log ("", G_LOG_LEVEL_INFO,
                         (_("[%s] DRC Check: no errors while checking package %s.")),
