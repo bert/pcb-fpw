3909d2ffb466d1c3a67714097bbdf09dafb75d07
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-02-08 07:16:33 +0100

    CON_DIL: correction in DRC success message.

--- src/con_dil.c
+++ src/con_dil.c
@@ -680,7 +680,7 @@ con_dil_drc ()
                 }
         }
         /* No failures on DRC found. */
-        if (verbose || (result == EXIT_SUCCESS))
+        if (verbose && (result == EXIT_SUCCESS))
         {
                 g_log ("", G_LOG_LEVEL_INFO,
                         (_("[%s] DRC Check: no errors while checking package %s.")),
