319aa429a07544e7e88d5f64602017c32a9fb773
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-02-08 08:01:46 +0100

    SIL: correction in DRC success message.

--- src/sil.c
+++ src/sil.c
@@ -1,7 +1,7 @@
 /*!
  * \file src/sil.c
  *
- * \author Copyright 2007, 2008, 2009, 2010 by Bert Timmerman <bert.timmerman@xs4all.nl>
+ * \author Copyright 2007, 2008, 2009, 2010, 2011 by Bert Timmerman <bert.timmerman@xs4all.nl>
  *
  * \brief Functions for SIL footprints.
  *
@@ -638,7 +638,7 @@ sil_drc ()
                 }
         }
         /* No failures on DRC found. */
-        if (verbose || (result == EXIT_SUCCESS))
+        if (verbose && (result == EXIT_SUCCESS))
         {
                 g_log ("", G_LOG_LEVEL_INFO,
                         (_("[%s] DRC Check: no errors while checking package %s.")),
