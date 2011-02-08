170b7c517e98b178427b9be337ef5b56a0e2c542
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-02-08 07:37:11 +0100

    DIOMELF: correction in DRC success message.

--- src/diomelf.c
+++ src/diomelf.c
@@ -1,7 +1,7 @@
 /*!
  * \file src/diomelf.c
  *
- * \author Copyright 2007, 2008, 2009 by Bert Timmerman <bert.timmerman@xs4all.nl>
+ * \author Copyright 2007, 2008, 2009, 2010, 2011 by Bert Timmerman <bert.timmerman@xs4all.nl>
  *
  * \brief Functions for DIOMELF footprints.
  *
@@ -698,7 +698,7 @@ diomelf_drc ()
         }
         /*! \todo Create attributes here. */
         /* No failures on DRC found. */
-        if (verbose || (result == EXIT_SUCCESS))
+        if (verbose && (result == EXIT_SUCCESS))
         {
                 g_log ("", G_LOG_LEVEL_INFO,
                         (_("[%s] DRC Check: no errors while checking package %s.")),
