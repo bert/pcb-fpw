baccd1d4c390e8e62c1c7fe78b9514cb73ac6fcf
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-02-08 21:15:37 +0100

    DIOMELF: correction in DRC for silkscreen linewidth.

--- src/diomelf.c
+++ src/diomelf.c
@@ -625,7 +625,7 @@ diomelf_drc ()
          * the solder fillet area or worse). */
 
         /* Check for a reasonable silk line width. */
-        if (silkscreen_package_outline || (silkscreen_line_width == 0.0))
+        if (silkscreen_package_outline && (silkscreen_line_width == 0.0))
         {
                 if (verbose)
                 {
@@ -649,7 +649,7 @@ diomelf_drc ()
                         break;
                 }
                 case MIL:
-                if (silkscreen_line_width > 40.0)
+                if (silkscreen_package_outline && (silkscreen_line_width > 40.0))
                 {
                         if (verbose)
                         {
@@ -661,7 +661,7 @@ diomelf_drc ()
                         break;
                 }
                 case MIL_100:
-                if (silkscreen_line_width > 4000.0)
+                if (silkscreen_package_outline && (silkscreen_line_width > 4000.0))
                 {
                         if (verbose)
                         {
@@ -673,7 +673,7 @@ diomelf_drc ()
                         break;
                 }
                 case MM:
-                if (silkscreen_line_width > 1.0)
+                if (silkscreen_package_outline && (silkscreen_line_width > 1.0))
                 {
                         if (verbose)
                         {
