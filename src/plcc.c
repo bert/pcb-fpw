92476ea2c288ceaa622c7cce2435f38ee3e25015
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-02-09 23:03:56 +0100

    PLCC: correction in DRC for silkscreen linewidth.

--- src/plcc.c
+++ src/plcc.c
@@ -759,7 +759,7 @@ plcc_drc ()
          * the solder fillet area or worse). */
 
         /* Check for a reasonable silk line width. */
-        if (silkscreen_package_outline || (silkscreen_line_width == 0.0))
+        if (silkscreen_package_outline && (silkscreen_line_width == 0.0))
         {
                 if (verbose)
                 {
@@ -783,7 +783,7 @@ plcc_drc ()
                         break;
                 }
                 case MIL:
-                if (silkscreen_line_width > 40.0)
+                if (silkscreen_package_outline && (silkscreen_line_width > 40.0))
                 {
                         if (verbose)
                         {
@@ -795,7 +795,7 @@ plcc_drc ()
                         break;
                 }
                 case MIL_100:
-                if (silkscreen_line_width > 4000.0)
+                if (silkscreen_package_outline && (silkscreen_line_width > 4000.0))
                 {
                         if (verbose)
                         {
@@ -807,7 +807,7 @@ plcc_drc ()
                         break;
                 }
                 case MM:
-                if (silkscreen_line_width > 1.0)
+                if (silkscreen_package_outline && (silkscreen_line_width > 1.0))
                 {
                         if (verbose)
                         {
