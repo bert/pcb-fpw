4ccca3bfdf1c7ec04f755416ed98112a94523e02
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-02-11 10:56:14 +0100

    RESC: correction in DRC for silkscreen linewidth.

--- src/resc.c
+++ src/resc.c
@@ -529,7 +529,7 @@ resc_drc ()
          * the solder fillet area or worse). */
 
         /* Check for a reasonable silk line width. */
-        if (silkscreen_package_outline || (silkscreen_line_width == 0.0))
+        if (silkscreen_package_outline && (silkscreen_line_width == 0.0))
         {
                 if (verbose)
                 {
@@ -553,7 +553,7 @@ resc_drc ()
                         break;
                 }
                 case MIL:
-                if (silkscreen_line_width > 40.0)
+                if (silkscreen_package_outline && (silkscreen_line_width > 40.0))
                 {
                         if (verbose)
                         {
@@ -565,7 +565,7 @@ resc_drc ()
                         break;
                 }
                 case MIL_100:
-                if (silkscreen_line_width > 4000.0)
+                if (silkscreen_package_outline && (silkscreen_line_width > 4000.0))
                 {
                         if (verbose)
                         {
@@ -577,7 +577,7 @@ resc_drc ()
                         break;
                 }
                 case MM:
-                if (silkscreen_line_width > 1.0)
+                if (silkscreen_package_outline && (silkscreen_line_width > 1.0))
                 {
                         if (verbose)
                         {
