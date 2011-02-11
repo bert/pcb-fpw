8dbaecbc2b24a592fd89d074f4205e2bf3d5dc26
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-02-11 11:55:04 +0100

    RESM: removed a dangling doxygen todo comment.

--- src/resm.c
+++ src/resm.c
@@ -690,7 +690,6 @@ resm_drc ()
                         break;
                 }
         }
-        /*! \todo Create attributes here. */
         /* No failures on DRC found. */
         if (verbose && (result == EXIT_SUCCESS))
         {
