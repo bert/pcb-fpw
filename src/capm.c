c4eb401d61a4d408cc69830e8efcac1530514346
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-02-27 22:07:28 +0100

    CAPM: removed the function call parameter of capm_get_default_footprint_values().

--- src/capm.c
+++ src/capm.c
@@ -717,11 +717,7 @@ capm_drc ()
  * found, \c EXIT_FAILURE when the footprint name was not found.
  */
 int
-capm_get_default_footprint_values
-(
-        gchar *footprint_name
-                /*!< : a \c NULL terminated footprint name.*/
-)
+capm_get_default_footprint_values ()
 {
         if (!strcmp (footprint_name, "?CAPM"))
         {
--- src/capm.h
+++ src/capm.h
@@ -30,7 +30,7 @@
 int capm_create_element ();
 int capm_create_packages_list ();
 int capm_drc ();
-int capm_get_default_footprint_values (gchar *);
+int capm_get_default_footprint_values ();
 #if GUI
 int capm_set_gui_constraints (GtkWidget *);
 #endif /* GUI */
