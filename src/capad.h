642c2cbbe0bfdfe22276ec81a728891ed6b23e74
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-04-08 08:13:45 +0200

    CAPAD: added code for CAPAD packages (Through Hole Capacitor, Non Polarized, Axial Diameter, Horizontal Mounted).

---
+++ src/capad.h
@@ -0,0 +1,44 @@
+/*!
+ * \file src/capad.h
+ *
+ * \author Copyright 2007, 2008, 2009, 2010, 2011 by Bert Timmerman <bert.timmerman@xs4all.nl>
+ *
+ * \brief Header file for functions for CAPAD footprints
+ *  (Through Hole Capacitor, Non Polarized, Axial Diameter, Horizontal Mounted).
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.\n
+ * \n
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.\n
+ * \n
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.\n
+ */
+
+
+#ifndef __CAPAD_INCLUDED__
+#define __CAPAD_INCLUDED__
+
+
+int capad_create_element ();
+int capad_create_packages_list ();
+int capad_drc ();
+int capad_get_default_footprint_values ();
+#if GUI
+int capad_set_gui_constraints ();
+#endif /* GUI */
+int capad_write_footprint ();
+void capad_init ();
+
+
+#endif /* __CAPAD_INCLUDED__ */
+
+
+/* EOF */
