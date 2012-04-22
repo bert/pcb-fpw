56652f7f8124732b7ac15b38fb4a750f78559124
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-22 11:43:04 +0200

    Moved the files for the Doxygen developer documentation to a separate directory.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- doc/doxygen/create_fpw-dox.sh
+++ doc/doxygen/create_fpw-dox.sh
@@ -4,4 +4,4 @@ echo '<link href="doxygen.css" rel="stylesheet" type="text/css">' >> header.html
 echo '<link href="tabs.css" rel="stylesheet" type="text/css">' >> header.html
 echo '</head><body>' >> header.html
 echo '<h3>PCB-FPW developer documentation based on version: ' $(git describe) ' </h3>' >> header.html
-doxygen fpw-dox-with-call-graphs.dox
+doxygen pcb-fpw.dox
