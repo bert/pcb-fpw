938b129d28b1dc6ba64d821f7f18feae9bc8c0e1
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-07-24 22:19:17 +0200

    Modified the header of the doxygen documentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- doc/doxygen/create_fpw_dox.sh
+++ doc/doxygen/create_fpw_dox.sh
@@ -3,7 +3,16 @@ echo '<title>PCB-FPW: PCB FPW - Printed Circuit Board FootPrintWizard.</title>' 
 echo '<link href="doxygen.css" rel="stylesheet" type="text/css">' >> header.html
 echo '<link href="tabs.css" rel="stylesheet" type="text/css">' >> header.html
 echo '</head><body>' >> header.html
-echo '<h3>PCB-FPW developer documentation based on version: ' $(git describe) ' </h3>' >> header.html
+echo '<table cellspacing="0" cellpadding="0">' >> header.html
+echo ' <tbody>' >> header.html
+echo ' <tr style="height: 48px;">' >> header.html
+echo '  <td id="projectlogo"><img alt="Logo" src="pcb-fpw_48x48.png"></td>' >> header.html
+echo '  <td style="padding-left: 0.5em;">' >> header.html
+echo '   <div id="projectname">PCB-FPW<span id="projectnumber">' $(git describe) 'developer documentation.</span></div>' >> header.html
+echo '  </td>' >> header.html
+echo ' </tr>' >> header.html
+echo ' </tbody>' >> header.html
+echo '</table>' >> header.html
 doxygen pcb-fpw.dox
 echo 'Created doxygen documentation for pcb-fpw ...'
 
