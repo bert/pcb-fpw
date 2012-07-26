28c0aeefd8b6f95b3fef6d1003934b76a28100fa
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-07-26 13:33:25 +0200

    GTK3: added pcb-gfpw.h.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ src/gtk3/pcb-gfpw.h
@@ -0,0 +1,41 @@
+/*!
+ * \file src/gtk3/pcb-gfpw.h
+ *
+ * \author Copyright 2007 ... 2012 by Bert Timmerman <bert.timmerman@xs4all.nl>
+ *
+ * \brief Header file for pcb-gfpw (GTK3 UI).
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.\n
+ * \n
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.\n
+ * \n
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.\n
+ */
+
+
+#ifndef __PCB_GFPW_INCLUDED__
+#define __PCB_GFPW_INCLUDED__
+
+
+#include <gtk/gtk.h>
+#include "about_dialog.h"
+
+
+int
+main (int argc, char *argv[]);
+
+
+#endif /* __PCB_GFPW_INCLUDED__ */
+
+
+/* EOF */
+
