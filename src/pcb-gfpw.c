02ec895a19e0da97ca53c0e36810459e0e40697c
Bert Timmerman <bert.timmerman@xs4all.nl>
2013-09-29 20:06:21 +0200

    Correction of the pcb homepage URI.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- src/pcb-gfpw.c
+++ src/pcb-gfpw.c
@@ -1,13 +1,13 @@
 /*!
  * \file src/pcb-gfpw.c
  *
- * \author Copyright (C) 2007, 2008, 2009, 2010, 2011 by Bert Timmerman <bert.timmerman@xs4all.nl>
+ * \author Copyright (C) 2007-2013 by Bert Timmerman <bert.timmerman@xs4all.nl>
  *
  * \brief GTK program for the pcb footprintwizard (pcb-gfpw).
  *
  * pcb-gfpw (GtkFootPrintWizard) is a program for the creation of footprint
  * files to be used by with the pcb layout application
- * (see http://pcb.gpleda.org) for the placement of parts in a pcb
+ * (see http://pcb.geda-project.org) for the placement of parts in a pcb
  * layout.\n
  *
  * This program is free software; you can redistribute it and/or modify
